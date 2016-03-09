1c43867055deb4a3b2a9f05b8a23ea228a099d4b
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 21:06:19 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -6,6 +6,31 @@
 
 	<h2>Sondages d'RPNix.com</h2>
 	
+	<ul id="categories">
+		<li class="forum_category">
+		<p>Votes Publics</p>
+		
+		<table class="forum" width="100%" cellspacing="0" cellpadding="0">
+			<tbody>
+				<tr class="head_table">
+					<th>Intitulé</th>
+					<th class="last_post">Créé par</th>
+				</tr>
+				<tr>
+					<td class="read">
+					<a href="#"> Sondage A</a>
+					</td>
+					<td>
+					<img width="20px" src="pics/avatar/miniskin_no.png">
+					<a class="name7-T" href="#"> Opérateur Etzu</a>
+					<br>
+					Le 26/02/2016 à 22:29
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		</li>
+	</ul>
 	
 <?php
 
