e0631ca43c724ca0bbdbfa0bad432779533b7533
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 22:02:38 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -5,6 +5,39 @@
 
 
 	<h2>Sondages d'RPNix.com</h2>
+
+<?php if (isset($_GET['s']))
+	
+	{
+	?>
+	<center>
+		<table width="100%" cellspacing="0" cellpadding="0">
+			<tbody>
+				<tr>
+					<td>
+						<img src="includes/img/magiepapertop.png" alt=" " />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table background="includes/img/magiepapercenter.png">
+						
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="includes/img/magiepapebottom.png" alt="" />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</center>
+	<?php
+	}
+	else
+	{
+?>
 	
 	<ul id="categories">
 		<li class="forum_category">
@@ -31,6 +64,9 @@
 		</table>
 		</li>
 	</ul>
+<?php
+	}
+?>
 	
 <?php
 
