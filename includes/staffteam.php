342b36af35d9a95b479004d8a61c2133223deffa
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 18:38:22 +0100

    Update staffteam.php

--- includes/staffteam.php
+++ includes/staffteam.php
@@ -7,24 +7,24 @@
 	<p>Sur cette page nous regrouperons les Membres du Staff ainsi que les anciens membres !</p>
 	<p>Cela pourra également vous donner une idée de la personnalité de chacuns, il est toujours agréable de savoir avec qui vous jouez.</p>
 	
-	<table id="member">
-	<th>Opérateurs</th>
-	<tr class="member_top">
+	<table cellspacing="0" cellpadding="0">
+	<th class="member_top">Opérateurs</th>
+	<tr class="memberbg_7">
 	<td><img src="pics/staff_team/Ange_Modo_1.png" alt="Opératrice Ange" /></td>
 	<td><span style="color:red">Ange</span><br />14 ans<br />France</td>
 	<td>"Rien n'est impossible, quand nous ne sommes pas seul."</td>
 	</tr>
-	<tr class="member_top">
+	<tr class="memberbg_7">
 	<td><img src="pics/staff_team/Balka_MJ_1.png" alt="Opérateur Balka" /></td>
 	<td><span style="color:red">Balka</span><br />15 ans<br />France<br />"Biscotte"</td>
 	<td>"Finissons comme nous avons commencé , ensemble !"</td>
 	</tr>
-	<tr class="member_top">
+	<tr class="memberbg_7">
 	<td><img src="pics/staff_team/Eftarthadeth_OP_1.png" alt="Opérateur Eftarthadeth" /></td>
 	<td><span style="color:red">Eftarthadeth</span><br />18 ans<br />Normandie, France</td>
 	<td>"Il n'y a si petit buisson qui ne porte son ombre, ceci est l'adéquation entre la réalité et l'Homme qui la pense."</td>
 	</tr>
-	<tr class="member_top">
+	<tr class="memberbg_7">
 	<td><img src="pics/staff_team/Etzu_OP_1.png" alt="Opérateur Etzu" /></td>
 	<td>
 		<span style="color:red">Etzu</span>
@@ -40,7 +40,7 @@
 	Mais elle doit être détruite, les feuilles, les branches, le tronc et les racines, combattue sans répit pour l'empêcher de se propager et de nous engloutir définitivement, car ce jour-là nous serions condamnés."<br/>
 	</td>
 	</tr>
-	<tr class="memberbg_5">
+	<tr class="memberbg_7">
 	<td><img src="pics/staff_team/Nikho_OP_1.png" alt="Opérateur Nikho" /></td>
 	<td><span style="color:red">Nikho</span><br/>18 ans <br />Normandie, France<br />"Le Boulet"</td>
 	<td>"Il n'existe aucun phénomène dont on ne peut trouver une explication ! L'investissement paye toujours."</td>
@@ -48,101 +48,101 @@
 	
 	<th class="member_top">Maîtres du Jeu</th>
 
-	<tr>
+	<tr class="memberbg_6">
 	<td><img src="pics/staff_team/Draug_MJ_1.png" alt="Maître Draug" /></td>
 	<td><span style="color:red">Draug</span><br />16 ans<br />"L'informé"</td>
 	<td>"Mangez du lard."</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_6">
 	<td><img src="pics/staff_team/Grim_MJ_1.png" alt="Maître Grim" /></td>
 	<td><span style="color:red">Grim</span><br />Toulouse, France</td>
 	<td>"Le monstre sous le lit ? c'est moi."</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_6">
 	<td><img src="pics/staff_team/Kuda_Modo_1.png" alt="Maitresse Kuda" /></td>
 	<td><span style="color:red">Kuda</span><br />16 ans<br />Neuchâtel, Suisse<br />"Grammar Nazie"</td>
 	<td>"Si ce que tu as à dire n'est pas plus beau que le silence, tait toi."</td>
 	</tr>
 	
-	<th>Modérateurs</th>
+	<th class="member_top">Modérateurs</th>
 	
 	<tr>
-	<td><img src="pics/staff_team/Zimon_Modo_1.png" alt="Modérateur Zimon" /></td>
+	<td class="memberbg_5"><img src="pics/staff_team/Zimon_Modo_1.png" alt="Modérateur Zimon" /></td>
 	<td><span style="color:green">Zimon</span><br />15 ans<br />Toulouse, France<br />"Le roux"</td>
 	<td>"No pasaran !"</td>
 	</tr>
 	
-	<th>Anciens Opérateurs</th>
-	<tr>
+	<th class="member_top">Anciens Opérateurs</th>
+	<tr class="memberbg_6">
 	<td><img src="pics/staff_team/Langa_MJ_1.png" alt="Dignitaire Langä" /></td>
 	<td><span style="color:red">Langä</span><br />France</td>
 	<td> </td>
 	</tr>
 	
-	<th>Anciens Maîtres du Jeu</th>
+	<th class="member_top">Anciens Maîtres du Jeu</th>
 	
-	<tr>
+	<tr class="memberbg_5">
 	<td><img src="pics/staff_team/Anateh_MJ_1.png" alt="Dignitaire Anateh" /></td>
 	<td><span style="color:red">Anateh</span><br />14 ans<br />Paris, France<br />"Norowette"</td>
 	<td>"Quand le masque tombe, le démon entre en scène"</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_5">
 	<td><img src="pics/staff_team/Herborus_MJ_1.png" alt="Difnitaire Herborus" /></td>
 	<td><span style="color:red">Herborus</span><br />16 ans<br />Belgique</td>
 	<td>"La construction, c'est ma passion "</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_5">
 	<td><img src="pics/staff_team/Karial_MJ_1.png" alt="Ex-MJ Karial" /></td>
 	<td><span style="color:red">Karial</span><br />Auvergne, France<br />"Kariou"</td>
 	<td>"Ce fut un plaisir énorme que de jouer à vos côtés"</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_5">
 	<td><img src="pics/staff_team/Koshoff_MJ_1.png" alt="Dignitaire Koshoff" /></td>
 	<td><span style="color:red">Koshoff</span><Br />France</td>
 	<td> </td>
 	</tr>
 	<tr>
-	<td><img src="pics/staff_team/Vahlm_MJ_1.png" alt="Dignitaire Vahlm" /></td>
+	<td class="memberbg_5"><img src="pics/staff_team/Vahlm_MJ_1.png" alt="Dignitaire Vahlm" /></td>
 	<td><span style="color:red">Vahlm</span><br />Suisse</td>
 	<td> </td>
 	</tr>
-	<tr>
+	<tr class="memberbg_5">
 	<td><img src="pics/staff_team/Shaolern_MJ_1.png" alt="Dignitaire Shaolern" /></td>
 	<td><span style="color:red">Shaolern</span><br />15 ans</td>
 	<td>"Après tout, le monde sur lequel vous évoluez n'est qu'un ramassis d'équations. Donc le réel dieu de ce monde, c'est moi!"</td>
 	</tr>
 	<tr>
-	<td><img src="pics/staff_team/Zelenan_MJ_1.png" alt="Dignitaire Zelenan" /></td>
+	<td class="memberbg_5"><img src="pics/staff_team/Zelenan_MJ_1.png" alt="Dignitaire Zelenan" /></td>
 	<td><span style="color:red">Zelenan</span><br />19 ans<br />Normandie, France<br />"Pro'Vanish"</td>
 	<td> </td>
 	</tr>
-	<th>Anciens Modérateurs</th>
-	<tr>
+	<th class="member_top">Anciens Modérateurs</th>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Abusos_Modo_1.png" alt="Dignitaire Abusos" /></td>
 	<td><span style="color:green">Abusos</span><Br />France</td>
 	<td> </td>
 	</tr>
-	<tr>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Eirik_Modo_1.png" alt="Dignitaire Eirik" /></td>
 	<td><span style="color:green">Eirik</span><br />16 ans<br />Bourgogne, France</td>
 	<td>"Peu importe le prix a payer ! Je trouverai la verité !"</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Einar_MJ_2.png" alt="Dignitaire Einar" /></td>
 	<td><span style="color:green">Einar</span><Br />Normandie, France</td>
 	<td>"La vérité est la croyance du naïf !"</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Diablox_Modo_3.png" alt="Modérateur Mephisto" /></td>
 	<td><span style="color:green">Méphisto</span><br />16 ans<br />France<br />"l'Ours"</td>
 	<td> </td>
 	</tr>
-	<tr>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Mysterios_Modo_2.png" alt="Modérateur Odium" /></td>
 	<td><span style="color:green">Odium</span><br />12 ans<br />Bretagne, France<br />"Le jeune"</td>
 	<td>"Si tu poses une question, tu passes pour un abruti le temps qu'on te réponde. Mais si tu poses pas la question, tu reste un abruti toute ta vie."</td>
 	</tr>
-	<tr>
+	<tr class="memberbg_4">
 	<td><img src="pics/staff_team/Tristan_Modo_1.png" alt="Dignitaire Tristan" /></td>
 	<td><span style="color:green">Tristan</span><Br />France</td>
 	<td> </td>
