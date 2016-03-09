0d43bbecea6b598a9ed71f77f52c082fba75a099
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 23:00:01 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -9,7 +9,7 @@
 <?php if (isset($_GET['s']))
 	{
 		$sondage = intval($_GET['s']);
-		$answer = $db->prepare('SELECT s.id AS s_id, s.sender_id AS sender, s.text, s.rank AS level, s.title AS titre, m.id AS id, m.name, m.rank AS rank, m.technician, m.pionier
+		$answer = $db->prepare('SELECT s.id AS s_id, s.sender_id AS sender, s.text, s.rank AS level, s.title AS titre, m.id AS id, m.name, m.title, m.rank AS rank, m.technician, m.pionier
 		FROM sondage s
 		RIGHT JOIN members m ON m.id = s.sender_id
 		WHERE s.id = ? ');
