f4bc87bb77bfe73d99398fc3c6aa2f818fc75425
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 18:08:19 +0100

    Update update.php

--- includes/update.php
+++ includes/update.php
@@ -8,8 +8,26 @@
 	
 	<p>Ici vous sera expliqué les travaux effectués et les tâches terminée de la maintenance actuelle</p>
 	
+	<p>Tâches à effectuer :</p>
 	<ul>
-		
+		<li>Mettre à jour les permissions de la Page Personnage</li>
+		<li>Mettre à jour les permissions des Pages de Modération</li>
+		<li>Mettre à jour les permissions des Pages d'Administration</li>
+		<li>Recréer le CSS du site et si possible l'améliorer au mieux</li>
+		<li>Mettre à jour les permissions de la Page Incantations</li>
+		<li>Mettre à jour les permissions de la Page Candidatures</li>
+		<li>Réadapter la PAge d'Acceuil aux évènements passés IG</li>
+	</ul>
+	
+	<p>Tâches effectuées :</p>
+	<ul>
+		<li>Mettre à jour les permissions de la Page Chatbox</li>
+		<li>Mettre à jour les Permissions de la Page Membres</li>
+		<li>Mettre à jour les permissions de la Page Forum</li>
+		<li>Mettre à jour les permissions de la Page News</li>
+		<li>Ajouter le Grade Encadrant</li>
+		<li>Mettre à jour le contenu de la Page Règles</li>
+		<li>Ajout du descriptif de la Race Zaknafein</li>
 	</ul>
 <?php
 
