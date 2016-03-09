3eaf622ea154524e59c74e851d6c0d116201e904
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-09 18:12:13 +0100

    Update update.php

--- includes/update.php
+++ includes/update.php
@@ -9,7 +9,7 @@
 	<p>Ici vous sera expliqué les travaux effectués et les tâches terminée de la maintenance actuelle</p>
 	
 	<p>Tâches à effectuer :</p>
-	<ul syle="color: red;">
+	<ul syle="color:red;">
 		<li>Mettre à jour les permissions de la Page Personnage</li>
 		<li>Mettre à jour les permissions des Pages de Modération</li>
 		<li>Mettre à jour les permissions des Pages d'Administration</li>
