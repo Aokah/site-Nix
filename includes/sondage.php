71798fa8c19e3b56c7d55ded7e174c9637d04799
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-15 00:27:50 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -121,6 +121,75 @@
 			</tbody>
 		</table>
 		</li>
+		<li class="forum_category">
+		<p>Votes Maitres du Jeu</p>
+		
+		<table class="forum" width="100%" cellspacing="0" cellpadding="0">
+			<tbody>
+				<tr class="head_table">
+					<th>Intitulé</th>
+					<th class="last_post">Créé par</th>
+				</tr>
+				<tr>
+					<td class="read">
+					<a href="#"> Sondage A</a>
+					</td>
+					<td>
+					<img width="20px" src="pics/avatar/miniskin_no.png">
+					<a class="name7-T" href="#"> Opérateur Etzu</a>
+					<br>
+					Le 26/02/2016 à 22:29
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		</li>
+		<li class="forum_category">
+		<p>Votes Publics</p>
+		
+		<table class="forum" width="100%" cellspacing="0" cellpadding="0">
+			<tbody>
+				<tr class="head_table">
+					<th>Intitulé</th>
+					<th class="last_post">Créé par</th>
+				</tr>
+				<tr>
+					<td class="read">
+					<a href="#"> Sondage A</a>
+					</td>
+					<td>
+					<img width="20px" src="pics/avatar/miniskin_no.png">
+					<a class="name7-T" href="#"> Opérateur Etzu</a>
+					<br>
+					Le 26/02/2016 à 22:29
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		</li>
+		<li class="forum_category">
+		<p>Votes Opérateurs</p>
+		
+		<table class="forum" width="100%" cellspacing="0" cellpadding="0">
+			<tbody>
+				<tr class="head_table">
+					<th>Intitulé</th>
+					<th class="last_post">Créé par</th>
+				</tr>
+				<tr>
+					<td class="read">
+					<a href="#"> Sondage A</a>
+					</td>
+					<td>
+					<img width="20px" src="pics/avatar/miniskin_no.png">
+					<a class="name7-T" href="#"> Opérateur Etzu</a>
+					<br>
+					Le 26/02/2016 à 22:29
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		</li>
 	</ul>
 <?php
 	}
