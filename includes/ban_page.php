2394de8c8af5e79baf29639f36623adb3a3f7d56
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-14 11:04:52 +0100

    Update ban_page.php

--- includes/ban_page.php
+++ includes/ban_page.php
@@ -1,7 +1,12 @@
 <?php function ban_page ()
 {
+  global $db, $_POST, $_SESSION, $_GET;
+  
+  $_SESSION['connected'] = false;
 ?>
-  pouet t ban
+  <h2>Vous avez été banni de ce site</h2>
+  
+  <p>Votre compte a été banni du site, probablement car vous ne respectiez pas les règles en vigueur ici.</p>
 <?php
 }
 ?>
