88af923338224d6de158f47982f41eb90f77a8d7
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-15 23:12:25 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -110,7 +110,7 @@
 									</td>
 								</tr>
 								<tr>
-									<td colspan="3" style="text-align:center;" style="padding-left:2%; padding-right:2%;">
+									<td colspan="3" style="text-align:center; padding:3%;" >
 										<?php echo $text ?>
 									</td>
 								</tr>
