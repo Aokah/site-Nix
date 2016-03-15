f50da276161c5d57b053fabf411d0e666b8b49f2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-15 23:46:54 +0100

    Update update.php

--- includes/update.php
+++ includes/update.php
@@ -37,6 +37,7 @@
 		<li>Réatribution des couleurs avec leur grade</li>
 		<li>Ajout de la Page Update</li>
 		<li>Mise à jour de la Page Staff Team</li>
+		<li>Ajout de la page des Sondages</li>
 		<li>Ajout de la fonction de bannissement <span style="color:red;">(A améliorer)</span>.</li>
 		<li>Insertion de la ChatBox sous les pages<span style="color:red;">(A améliorer)</span>.</li>
 	</ul>
