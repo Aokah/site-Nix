2e02ce4851598c8b86ed8556e964bc0100e654d3
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-15 18:53:14 +0100

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -9,9 +9,8 @@
 <?php if (isset($_GET['s']))
 	{
 		$sondage = intval($_GET['s']);
-	$verif = $db->prepare('SELECT s.id, s.sondage_id AS sondage, s.sender_id, s.vote, m.id AS m_id,
+	$verif = $db->prepare('SELECT s.id, s.sondage_id AS sondage, s.sender_id, s.vote
 				FROM sondage_votes s
-				RIGHT JOIN members m ON m.id = s.sender_id
 				WHERE s.sondage_id = 1 AND s.sender_id = 10');
 	$verif->execute(array($sondage, $_SESSION['id']));
 	if (isset($_GET['v']) && $_GET['v'] == 'pour')
