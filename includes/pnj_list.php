199242b2aa52fb49d048f6d9dc2952531f402393
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-16 16:20:26 +0100

    Update pnj_list.php

--- includes/pnj_list.php
+++ includes/pnj_list.php
@@ -167,9 +167,9 @@
   				if (isset($_POST['end']))
   					{
   						$edit = $db->prepare('UPDATE pnj_list SET role = ?, prenom = ?, nom = ?, origine = ?, race = ?, taille = ?,
-  						poids = ?, sd=  ?, element = ?, qualite = ?, \'default\' = ?, \'event\' = ?, caractere = ?, equipement = ?, bg = ? WHERE id = ?');
+  						poids = ?, sd=  ?, element = ?, qualite = ?, caractere = ?, equipement = ?, bg = ? WHERE id = ?');
   						$edit->execute(array($role, $prenom, $nom, $origine, $race, $taille, $poids, $sd, $element, $qualite,
-  						$defaut, $event, $caractere, $equipement, $bg, $id));
+  						 $caractere, $equipement, $bg, $id));
   						?>
   						<p>Votre page a bien été modifiée !</p> 
   						<p><a href = "index?p=pnj_list&pnj=<?php echo $id; ?>">Cliquez ici</a> Pour accéder à la page.</p>
