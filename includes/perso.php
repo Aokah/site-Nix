74a5d15f663a56b2f18afff194e61ec34172f29c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-19 19:07:41 +0100

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -1345,7 +1345,7 @@
 			</tr>
 			<?php if ($line['valid_bg'] == 1) {
 				
-				$valier = $db->prepare('SELECT name, id, title FROM members WHERE id = ?');
+				$valier = $db->prepare('SELECT * FROM members WHERE id = ?');
 				$valider->execute(array($line['valider_id']));
 				if ($bgline = $valider->fecth())
 				{
