8644b33a1170c23841a7a8d32543e63db54c320e
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-21 10:46:44 +0100

    Update members.php

--- includes/members.php
+++ includes/members.php
@@ -1,10 +1,22 @@
 <?php function members ()
 {
-	global $db, $_SESSION;
+	global $db, $_SESSION, $_GET;
 	
-	if ($_SESSION['rank'] > 5) {
-		
-		
+	if (isset($_GET['search'])) {
+	
+	$search = intval($_GET['search']);
+	$page = $db->prepare('SELECT * FROM members WHERE name = ? ORDER BY rank DESC, name ASC');
+	$page->execute(array($search));
+	
 	}
-	else { echo '<p>Maintenance en cours.</p>'; }
+	else
+		
+	{
+		$linerank = 10;
+		while ($linerank >= 0)
+		{
+			echo '<p>Tableau du rang '. $linerank .'.</p>';
+			$linerank--;
+		}
+	}
 } ?>
