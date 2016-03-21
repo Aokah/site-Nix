fcb0b9d43dc80f3ba995a7e213452d464bf73724
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-21 10:54:16 +0100

    Update members.php

--- includes/members.php
+++ includes/members.php
@@ -14,8 +14,15 @@
 	{
 		$linerank = 10;
 		while ($linerank >= 0)
+			switch ($linerank)
+			{
+				case 10 : $linename = "Consciences"; break; case 9 : $linename = "Titans"; break; case 8: $linename = "Dieux"; break;
+				case 7: $linename = "Opérateurs"; break; case 6: $linename = "Maitres du Jeu"; break; case 5 : $linename = "Modérateurs"; break;
+				case 4: $linename = "Encadrants"; break; case 3 : $linename = "Joueurs Investis"; break; case 2 : $linename = "Joueurs"; break;
+				case 1: $linename = "Nouveaux"; break; case 0: $linename = "Comptes non validés"; break;
+			}
 		{
-			echo '<p>Tableau du rang '. $linerank .'.</p>';
+			echo '<p>Tableau du rang '. $linerank .' ('. $linename .').</p>';
 			$linerank--;
 		}
 	}
