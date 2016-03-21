f56bae5e7c41ba09ff9b70eeac84e04d54b9ee44
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-21 11:40:47 +0100

    Update members.php

--- includes/members.php
+++ includes/members.php
@@ -2,28 +2,77 @@
 {
 	global $db, $_SESSION, $_GET;
 	
-	if (isset($_GET['search'])) {
+	if ($_SESSION['rank'] >= 7) { $ranklimit = 10; } elseif ($_SESSION['rank'] == 6) { $ranklimit = 9;}
+	elseif ($_SESSIOON['rank'] == 5) { $ranklimit = 8; } else { $ranklimit = 7; }
 	
-	$search = intval($_GET['search']);
-	$page = $db->prepare('SELECT * FROM members WHERE name = ? ORDER BY rank DESC, name ASC');
-	$page->execute(array($search));
-	
+	if (isset($_GET['search']))
+	{
+		$search = intval($_GET['search']);
+		$page = $db->prepare('SELECT * FROM members WHERE name = ? AND rank >= ? ORDER BY rank DESC, name ASC');
+		$page->execute(array($ranklimit, $search));
 	}
 	else
-		
 	{
-		$linerank = 10;
-		while ($linerank >= 0)
+		$page = $db->prepare('SELECT * FROM members WHERE rank >= ? ORDER BY rank DESC, name ASC');
+		$page->execute(array($ranklimit));
+	}
+	
+	if ($line = $page->fetch())
+	{
+	?>
+		<table>
+			<tbody>
+		<?php
+		$linerank = $ranklimit;
+		while ($linerank >= 1)
+		{	
 			switch ($linerank)
 			{
 				case 10 : $linename = "Consciences"; break; case 9 : $linename = "Titans"; break; case 8: $linename = "Dieux"; break;
 				case 7: $linename = "Opérateurs"; break; case 6: $linename = "Maitres du Jeu"; break; case 5 : $linename = "Modérateurs"; break;
 				case 4: $linename = "Encadrants"; break; case 3 : $linename = "Joueurs Investis"; break; case 2 : $linename = "Joueurs"; break;
-				case 1: $linename = "Nouveaux"; break; case 0: $linename = "Comptes non validés"; break;
+				case 1: $linename = "Nouveaux"; break;
 			}
-		{
+			switch ($line['magie_rank'])
+			{
+				case 0: $level = "Profane"; break; case 1:$level = "Adepte"; break; case 2: $level = "Apprenti Magicien"; break;
+				case 3: $level = "Magicien"; break; case 4: $level = "Mage"; break; case 5: $level = "Archimage"; break;
+				case 6: $level = "Sage"; break; case 7: $level = "Divin"; break; case 8: $level = "Titanèsque"; break;
+				case 9: $level = "Suprême"; break;
+			}
+			?>
+			<tr>
+				<th><?php echo $linename; ?></th>
+				<th>Titre</th>
+				<th>BG</th>
+				<th>Spé'</th>
+				<th>Niv'</th>
+			</tr>
+			<tr>
+				<td>
+					<img src="pics/rank<?php echo $linerank; ?>.png" alt="" width="27" /> <img src="pics/avatar/miniskin_<?= $line['id'?>.png" alt="" /> <?= $line['name']?>
+				</td>
+				<td>
+					<?php echo $title; ?>
+				</td>
+				<td>
+					BG
+				</td>
+				<td>
+					<img src="pics/magie/Magie_<?= $line['specialisation']?>.png" alt="" class="magie" title="<?= $line['specialisation']?>"/> / <img src="pics/magie/Magie_<?= $line['spe_2']?>.png" alt="" title="<?= $line['spe_2']?>" class="magie" />
+				</td>
+				<td>
+					<img src="pics/magie_rank_<?= $line['magie_rank']?>.gif" alt="" title="Niveau <?pgp echo $level ;?>" />
+				</td>
+			</tr>
+			<?php
 			echo '<p>Tableau du rang '. $linerank .' ('. $linename .').</p>';
 			$linerank--;
 		}
+		?>
+			</tbody>
+		</table>
+	<?php
 	}
+	else { echo '<p>Une erreur s\'est produite.</p>'; }
 } ?>
