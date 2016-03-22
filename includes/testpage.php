8e14e4e36d9ddc83284cc9a5b0750a049e2a2d1c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-22 20:27:00 +0100

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -1,5 +1,10 @@
 <?php function testpage ()
 {
 
+global $_POST, $db, $_SESSION;
+
+  $max = $db->query('SELECT COUNT(*) AS idmax FROM members WHERE magie_rank < 7'); $max = $max->fetch();
+
+  echo $max['idmax'];
 }
 ?>
