d880ff8ca6560abcb8d0e6a131f57293b2c0c595
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-22 20:53:55 +0100

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -6,7 +6,6 @@ global $_POST, $db, $_SESSION;
   $max = $db->query('SELECT COUNT(*) AS idmax FROM members'); $max = $max->fetch();
   $idmax = $max['idmax'] ;
   
-  echo $idmax;
   $id = $idmax;
   while ($id > 0)
   {
@@ -16,7 +15,7 @@ global $_POST, $db, $_SESSION;
     
       if ($select = $select->fetch())
       {
-        echo 'PMs :' , $select['E_magique'], '<br />PVs :', $select['E_vitale'], '<br />level :' , $select['magie_rank'];
+        echo 'id = ', $id, '<br />PMs :' , $select['E_magique'], '<br />PVs :', $select['E_vitale'], '<br />level :' , $select['magie_rank'], '<br />';
       }
     $id --;
   }
