72864da152aa380d7d742ff2e09967a4516eca5c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-23 21:07:25 +0100

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -31,8 +31,8 @@ global $_POST, $db, $_SESSION;
         else
         { $add = 0; }
         $finaladd = $select['E_magique'] + $add;
-       // $maj = $db->prepare('UPDATE members SET E_magique = E_magique + ? WHERE id = ?');
-       // $maj->execute(array($add, $id));
+        $maj = $db->prepare('UPDATE members SET E_magique = E_magique + ? WHERE id = ?');
+        $maj->execute(array($add, $id));
        ?>
        <table>
          <tbody>
