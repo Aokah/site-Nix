395b464c948c990f37e435219abeda819335ef76
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-23 20:24:08 +0100

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -3,7 +3,7 @@
 
 global $_POST, $db, $_SESSION;
 
-  $max = $db->query('SELECT COUNT(*) AS idmax FROM members'); $max = $max->fetch();
+  $max = $db->query('SELECT COUNT(*) AS idmax FROM members WHERE id != 0'); $max = $max->fetch();
   $idmax = $max['idmax'] ;
   
   $id = $idmax;
