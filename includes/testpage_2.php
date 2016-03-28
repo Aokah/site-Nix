28e250a863e4396473c6841d64aa493ac6a526b2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-28 20:18:52 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -2,14 +2,14 @@
 {
   if(isset($_GET['perso']))
     {
-    $perso = intval($_GET['perso']);
-    $get = $db->prepare('SELECT h.id, h.sender_id, h.sender_rank, h.avis, h.target_id, m.id m_id, m.title, m.name
+    $perso = intval($_GET['perso']); echo $perso;
+    $avis = $db->prepare('SELECT h.id, h.sender_id, h.sender_rank, h.avis, h.target_id, m.id m_id, m.title, m.name
     FROM hrpavis h
     RIGHT JOIN members m
     ON m_id = h.target_id
     WHERE target_id = ?
     ORDER BY h.id DESC');
-    $get->execute(array($perso));
+    $avis->execute(array($perso));
     }
   else
   { echo 't\'as oublié de mettre une valeur, c\'est une page test hein !'; }
