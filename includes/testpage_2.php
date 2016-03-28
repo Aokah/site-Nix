e48656a07b370b04d6ee74536e1fa3e9bb1f3c16
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-28 20:21:12 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -5,8 +5,6 @@
     $perso = intval($_GET['perso']); echo $perso;
     $avis = $db->prepare('SELECT h.id, h.sender_id, h.sender_rank, h.avis, h.target_id, m.id AS m_id, m.title, m.name
     FROM hrpavis h
-    RIGHT JOIN members m
-    ON m_id = h.target_id
     WHERE target_id = ?
     ORDER BY h.id DESC');
     $avis->execute(array($perso));
