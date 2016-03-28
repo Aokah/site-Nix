0f03888aed7d307388561bf3e29cae1f50b45f09
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-28 20:19:44 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -3,7 +3,7 @@
   if(isset($_GET['perso']))
     {
     $perso = intval($_GET['perso']); echo $perso;
-    $avis = $db->prepare('SELECT h.id, h.sender_id, h.sender_rank, h.avis, h.target_id, m.id m_id, m.title, m.name
+    $avis = $db->prepare('SELECT h.id, h.sender_id, h.sender_rank, h.avis, h.target_id, m.id AS m_id, m.title, m.name
     FROM hrpavis h
     RIGHT JOIN members m
     ON m_id = h.target_id
