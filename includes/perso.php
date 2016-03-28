173f943d16418ca3a942e726a7398601d43b72c2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-29 00:24:31 +0200

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -38,7 +38,7 @@
 				</tr>
 				<tr>
 					<td>
-						<table width="640px" background="/pics/ico/magiepapercenter.png" style="padding:5%;">
+						<table width="640px" background="/pics/ico/magiepapercenter.png" style="padding:5%; text-align:center;">
 							<tbody>
 								<tr>
 									<th>Nom</th>
