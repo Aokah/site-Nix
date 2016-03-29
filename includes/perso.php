bd3b5386ba4ff049337cbe50b18f7d068f852ee7
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-29 21:04:13 +0200

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -877,6 +877,7 @@
 					$countj = $line['plus'] - $line2['moins'];
 					$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
 					$counts = $plus - $moins; $hrpavis = $countj + $counts;
+					if ($count => 10) { $coloravis => 10;} elseif ($count => 20) { $coloravis = 20;} elseif ($count => 30) { $coloravis = 30;} elseif ($count < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
 				?>
 				<td valign="top" width="50%" rowspan="<?php echo $span; ?>">
 					<table width="640px" cellspacing="5" cellpading="5" class="pnjtable" align="center">
@@ -909,8 +910,8 @@
 									Race : <?= $line['race']?>
 								</td>
 								<td colspan="2">
-									Qualité de jeu : <a href="index?p=perso&viewavis=<?php echo $perso;?>">[<?php echo $hrpavis;?>]</a> <?php 
-									if ($_SESSION['rank'] > 2) { ?><a href="index?p=perso&perso=<?php echo $perso;?>&action=avisok" class="ok">[+1]</a> <a href="index?p=perso&perso=<?php echo $perso;?>&action=avisko" class="ko">[-1]</a> 
+									Qualité de jeu : <a class="avis<?= $coloravis?>" href="index?p=perso&viewavis=<?php echo $perso;?>">[<?php echo $hrpavis;?>]</a> <?php 
+									if ($_SESSION['rank'] > 2 AND $_SESSION['id'] != $perso) { ?><a href="index?p=perso&perso=<?php echo $perso;?>&action=avisok" class="ok">[+1]</a> <a href="index?p=perso&perso=<?php echo $perso;?>&action=avisko" class="ko">[-1]</a> 
 									<?php } ?>
 								</td>
 							</tr>
