d6041977fd95a85bb2611cd544ae42bcba1e4f11
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-29 22:11:32 +0200

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -867,14 +867,14 @@
 				</td>
 				<?php if ($_SESSION['rank'] < 6 ) { $span = "2"; } else { $span = "3"; }
 					$select = $db->prepare('SELECT COUNT(*) AS plus FROM hrpavis WHERE target_id = ? AND avis = 1 AND sender_rank <= 4');
-					$select->execute(array($perso)); $line = $select->fetch();
+					$select->execute(array($perso)); $line0 = $select->fetch();
 					$select1 = $db->prepare('SELECT COUNT(*) AS plusstaff FROM hrpavis WHERE target_id = ? AND avis = 1 AND sender_rank > 4');
 					$select1->execute(array($perso)); $line1 = $select1->fetch();
 					$select2 = $db->prepare('SELECT COUNT(*) AS moins FROM hrpavis WHERE target_id = ? AND avis = 0 AND sender_rank <= 4');
 					$select2->execute(array($perso)); $line2 = $select2->fetch();
 					$select3 = $db->prepare('SELECT COUNT(*) AS moinsstaff FROM hrpavis WHERE target_id = ? AND avis = 0 AND sender_rank > 4');
 					$select3->execute(array($perso)); $line3 = $select3->fetch();
-					$countj = $line['plus'] - $line2['moins'];
+					$countj = $line0['plus'] - $line2['moins'];
 					$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
 					$counts = $plus - $moins; $hrpavis = $countj + $counts;
 					if ($hrpavis >= 10) { $coloravis = 10;} elseif ($hrpavis >= 20) { $coloravis = 20;} elseif ($hrpavis >= 30) { $coloravis = 30;} elseif ($hrpavis < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
