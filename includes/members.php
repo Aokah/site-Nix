11a6a4e8db7a9ff2d3a60460798d29be8d1d4fe0
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-29 21:43:06 +0200

    Update members.php

--- includes/members.php
+++ includes/members.php
@@ -48,6 +48,7 @@
 				<th>Niv'</th>
 				<th>Sorts</th>
 				<th>Msg</th>
+				<th>HRP</th>
 			<?php if ($_SESSION['rank'] >= 5) { ?> <th>Inv'</th> <?php } ?>
 				<th>Energie Magique/Vitale</th>
 			</tr>
@@ -93,6 +94,18 @@
 					if ($line['E_vitale'] >= 180 AND $line['E_vitale'] <= 199) 	{	$tvie = 90 ;	}
 					if ($line['E_vitale'] == 200) 	{	$tvie = 100 ;	}
 					$pmcount = ($line['magie_rank'] > 7) ? 'PMs Illimité !' : ''.$line['E_magique'].' PMs restants !' ;
+					$select = $db->prepare('SELECT COUNT(*) AS plus FROM hrpavis WHERE target_id = ? AND avis = 1 AND sender_rank <= 4');
+					$select->execute(array($line['id'])); $line0 = $select->fetch();
+					$select1 = $db->prepare('SELECT COUNT(*) AS plusstaff FROM hrpavis WHERE target_id = ? AND avis = 1 AND sender_rank > 4');
+					$select1->execute(array($line['id'])); $line1 = $select1->fetch();
+					$select2 = $db->prepare('SELECT COUNT(*) AS moins FROM hrpavis WHERE target_id = ? AND avis = 0 AND sender_rank <= 4');
+					$select2->execute(array($line['id'])); $line2 = $select2->fetch();
+					$select3 = $db->prepare('SELECT COUNT(*) AS moinsstaff FROM hrpavis WHERE target_id = ? AND avis = 0 AND sender_rank > 4');
+					$select3->execute(array($line['id'])); $line3 = $select3->fetch();
+					$countj = $line0['plus'] - $line2['moins'];
+					$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
+					$counts = $plus - $moins; $hrpavis = $countj + $counts;
+					if ($count >= 10) { $coloravis = 10;} elseif ($count >= 20) { $coloravis = 20;} elseif ($count >= 30) { $coloravis = 30;} elseif ($count < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
 					
 				switch ($line['magie_rank'])
 				{
@@ -140,6 +153,9 @@
 				</td>
 				<td style="text-align:center;">
 					<?= $fofo['msg']?>
+				</td>
+				<td>
+					<span class="avis<?= $coloravis?>"><?= $count?></span>
 				</td>
 			<?php if ($_SESSION['rank'] >= 5) { ?>
 				<td style="text-align:center;">
