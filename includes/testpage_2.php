6cb5afe7fd788c2866c39746082a5f797b7a54ff
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-02 14:19:15 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -10,12 +10,8 @@
   {
     if ($_GET['i'] == "valid")
     {
-    ?>
-    <h3>Vision du sort à l'unité (Modo +)</h3>
-    <?php
-    }
-    elseif ($_GET['i'] ==  "unvalid")
-    {
+      if ($_SESSION['rank'] > 4)
+        {
       $incan = $db->query('SELECT ig.id, ig.user_id, ig.incan_id, ig.valid,
       m.id AS m_id, m.rank, m.pionier, m.technician, m.ban, m.removed, m.name AS nom,
       il.id AS il_id, il.name AS sort, il.desc, il.level, il.cost, il.command, il.type
@@ -25,6 +21,30 @@
       WHERE ig.valid = 0
       ORDER BY nom ASC , il.level DESC , il.type ASC , il.name');
       $line = $incan->fetch();
+        }
+        else { echo 'Vous n\'avez pas le niveau pour voir ette partie de la page (bien tenté !)'; }
+    ?>
+    <h3>Liste des sorts Validés</h3>
+    <?php
+    }
+    elseif ($_GET['i'] ==  "unvalid")
+    {
+      if ($_SESSION['rank'] > 4)
+        {
+      $incan = $db->query('SELECT ig.id, ig.user_id, ig.incan_id, ig.valid,
+      m.id AS m_id, m.rank, m.pionier, m.technician, m.ban, m.removed, m.name AS nom,
+      il.id AS il_id, il.name AS sort, il.desc, il.level, il.cost, il.command, il.type
+      FROM incan_get ig
+      RIGHT JOIN members m ON m.id = ig.user_id
+      LEFT JOIN incan_list il ON il.id = ig.incan_id
+      WHERE ig.valid = 0
+      ORDER BY nom ASC , il.level DESC , il.type ASC , il.name');
+      $line = $incan->fetch();
+      ?>
+        <h3>Liste des sorts non validés</h3>
+      <?php
+        }
+        else { echo 'Vous n\'avez pas le niveau pour voir ette partie de la page (bien tenté !)'; }
     }
     else { echo 'N\'oublies pas de mettre une action valable :)'; }
   }
