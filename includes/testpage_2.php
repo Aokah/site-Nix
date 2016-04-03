fbb38a81e5841f2fb02f3c964adcde831598282d
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-03 15:49:57 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -20,6 +20,76 @@
     <h3>Liste des sorts Validés</h3>
     <?php 
     $irank = 8;
+    while ($irank > 0)
+    {
+      $incan = $db->prepare('SELECT ig.id, ig.user_id, ig.incan_id, ig.valid,
+      m.id AS m_id, m.rank, m.pionier, m.technician, m.ban, m.removed, m.name AS nom,
+      il.id AS il_id, il.name AS sort, il.desc, il.level, il.cost, il.command, il.type
+      FROM incan_get ig
+      RIGHT JOIN members m ON m.id = ig.user_id
+      LEFT JOIN incan_list il ON il.id = ig.incan_id
+      WHERE ig.valid = 0 AND il.level = ? AND nom = ?
+      ORDER BY nom ASC , il.level DESC , il.type ASC , il.name');
+      $incan->execute(array($irank,$perso));
+      $line = $incan->fetch();
+      
+      $name = $db->prepare('SELECT id, name FROM members WHERE name = ?');
+      $name->execute(array($perso)); $name = $name->fetch();
+      $id = $name['id'];
+      $select = $db->prepare('SELECT COUNT(*) AS verif FROM incan_get
+      RIGHT JOIN incan_list ON incan_list.id = incan_get.incan_id
+      WHERE incan_get.user_id = ? AND incan_list.level = ?');
+      $select->execute(array($id, $irank)); $count = $select->fetch();
+      echo $irank, $count['verif'];
+      if ($count['verif'] != 0)
+      {
+    ?>
+    <table cellspacing="0" cellpadding="0" align="center">
+      <tbody>
+        <tr>
+          <td>
+            <img src="pics/ico/magiepapertop.png" alt="" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table width="640px" background="/pics/ico/magiepapercenter.png" align="center">
+              <tbody>
+                <tr>
+                  <td style="text-align:center;">
+                    Formule
+                  </td>
+                </tr>
+                <tr>
+                   <td style="text-align:center;">
+                     Element / Niveau
+                  </td>
+                </tr>
+                <tr>
+                  <td style="text-align:center;">
+                    Desc
+                  </td>
+                </tr>
+                <tr>
+                  <td style="text-align:center;">
+                    coùt
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="/pics/ico/magiepapebottom.png" alt="">
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <?php
+    $irank-- ;
+    }
+    }
     
         }
         else { echo 'Vous n\'avez pas le niveau pour voir ette partie de la page (bien tenté !)'; }
