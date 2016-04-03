0e2d62d5746d50b8e63e7045abe4b8291c6e39e2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-03 17:47:58 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -23,7 +23,7 @@
     while ($irank > 0)
     {
       $incan = $db->prepare('SELECT ig.id, ig.user_id, ig.incan_id, ig.valid,
-      m.id AS m_id, m.rank, m.pionier, m.technician, m.ban, m.removed, m.name AS nom,
+      m.id AS m_id, m.rank, m.pionier, m.technician, m.ban, m.removed, m.name AS nom, m.title,
       il.id AS il_id, il.name AS sort, il.desc, il.level, il.cost, il.command, il.type
       FROM incan_get ig
       RIGHT JOIN members m ON m.id = ig.user_id
@@ -31,8 +31,12 @@
       WHERE ig.valid = 0 AND il.level = ? AND nom = ?
       ORDER BY nom ASC , il.level DESC , il.type ASC , il.name');
       $incan->execute(array($irank,$perso));
-      $line = $incan->fetch();
-      
+      $nom = $incan->fetch());
+      $tech = ($nom['technician']) ? '-T' : ''; $pionier = ($nom['pionier']) ? '-P' : '';
+      if ($nom['ban'] == 1) { $title = "Banni";} elseif ($nom['removed'] == 1) { $title = "Oublié";} else { $title = $nom['title'];}
+      ?>
+      <h4 class="name<?=$nom['rank'], $tech, $pionier?>"><?=$title ?> <?=$nom['nom']?></h4>
+      <?php
       $name = $db->prepare('SELECT id, name FROM members WHERE name = ?');
       $name->execute(array($perso)); $name = $name->fetch();
       $id = $name['id'];
@@ -40,10 +44,17 @@
       RIGHT JOIN incan_list ON incan_list.id = incan_get.incan_id
       WHERE incan_get.user_id = ? AND incan_list.level = ?');
       $select->execute(array($id, $irank)); $count = $select->fetch();
-      echo $irank, $count['verif'];
       if ($count['verif'] != 0)
       {
+        $incan = $db->prepare('SELECT ig.id ig.user_id, ig.incan_id, ig.valid,
+        il.id AS il_id, il.name, il.desc, il.type, il.cost, il.command
+        FROM incan_get ig
+        RIGHT JOIN incan_list il ON il.id = ig.incan_id
+        WHERE ig.user_id = ?
+        ORDER BY level DESC, typel ASC, name ASC');
+        $incan->execute(array($id));
     ?>
+    
     <table cellspacing="0" cellpadding="0" align="center">
       <tbody>
         <tr>
@@ -53,30 +64,52 @@
         </tr>
         <tr>
           <td>
+            <?php while ($line = $incan->fetch())
+            {
+              switch ($line['type'])
+              {
+                case 13: $type	= "Terre" ; break; case 12: $type = "Psy" ; break; case 11: $type = "Ombre" ; break; case 10:  $type = "Nature" ; break; case 9:  $type = "Métal" ; break;
+								case 8: $type = "Lumière" ; break; case 7: $type = "Glace" ; break; case 6: $type = "Feu" ; break; case 5: $type = "Energie" ; break;
+								case 4: $type = "Eau" ; break; case 3: $type = "Chaos" ; break; case 2: $type = "Arcane" ; break; case 1: $type = "Air" ; break; case 0: $type = "Inconnue" ; break; 
+              }
+              switch ($line['level'])
+              {
+                case 8: $level = "X"; break;	case 7:  $level = "S"; break; case 6:  $level = "A"; break; case 5:  $level = "B"; break; case 4:  $level = "C"; break; case 3:  $level = "D"; break; 
+								case 2:  $level = "E"; break; case 1:  $level = "F"; break;
+              }
+              ?>
             <table width="640px" background="/pics/ico/magiepapercenter.png" align="center">
               <tbody>
                 <tr>
                   <td style="text-align:center;">
-                    Formule
+                    <?= $line['name']?>
                   </td>
                 </tr>
                 <tr>
                    <td style="text-align:center;">
-                     Element / Niveau
+                     <img src="pics/magie/Magie_<?= $type?>.png" alt="" width="60" class="magie_type" /> <img src="pics/magie/Magie_<?= $level?>.png" alt="" width="60" class="magie" />
                   </td>
                 </tr>
                 <tr>
                   <td style="text-align:center;">
-                    Desc
+                    <?= $line['desc']?>
                   </td>
                 </tr>
                 <tr>
                   <td style="text-align:center;">
-                    coùt
+                    <?= $line['cost']?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="index?p=sorts&action=lunch&for=<?=$id?>" class="name5">[Lancer le sort !]</a>
                   </td>
                 </tr>
               </tbody>
             </table>
+            <?php
+            }
+            ?>
           </td>
         </tr>
         <tr>
