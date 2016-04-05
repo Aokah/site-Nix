ecf93eedca69a81123e609278a5ac03013662a18
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-05 15:38:23 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,8 +1,8 @@
 <?php function testpage_2()
 {	
 global $db;
-	$sort = 1;
-	$user = 1;
+	$sort = 79;
+	$user = 204;
 	
 	$select = $db->prepare('SELECT m.E_magique, m.E_vitale, m.id, ig.incan_id, ig.user_id, il.id AS sort, il.cost
 	FROM incan_get ig
@@ -10,6 +10,13 @@ global $db;
 	LEFT JOIN incan_list il ON ig.incan_id = il.id
 	WHERE ig.user_id = ? AND ig.incan_id = ?');
 	$select->execute(array($user, $sort));
+	$line = $select->fetch();
+	
+	$pm = $line['E_magique'];
+	$pv = $line['E_vitale'];
+	$cost = $line['cost'];
+	
+	echo $pm, ' ', $pv, ' ', $cost;
 	
 }
 ?>
