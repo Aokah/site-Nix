d66c358751846a79c62b448ef76bb2287b539413
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-05 15:32:41 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,10 +1,15 @@
 <?php function testpage_2()
-{
-	global $db;
-?>
-	<?php
+{	
+global $db;
+	$sort = 1;
+	$user = 1;
 	
-	?>
-<?php
+	$select = $db->prepare('SELECT m.E_magique, m.E_vitale, m.id, ig.incan_id, ig.user_id, il.id AS sort, il.cost
+	FROM incan_get ig
+	RIGHT JOIN members m ON m.id = ig.user_id
+	LEFT JOIN incan_list il ON ig.incan_id = il.id
+	WHERE ig.user_id = ? AND ig.incan_id = ?');
+	$select->execute(array($user, $sort));
+	
 }
 ?>
