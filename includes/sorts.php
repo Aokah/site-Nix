e8782b6b8e8f8784a2d2b49e8d262b7d0a7aba12
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-05 17:04:02 +0200

    Update sorts.php

--- includes/sorts.php
+++ includes/sorts.php
@@ -811,6 +811,13 @@
 		$verif->execute(array($_SESSION['id'])); $verif = $verif->fetch();
 		if ($verif['count'] != 0)
 		{
+			$verif = $db->query('SELECT COUNT(ig.incan_id, il.id, il.level) AS verif FROM incan_get ig
+			RIGHT JOIN ican_list il ON ig.incan_id = il.id
+			WHERE il.level > 6'); $verif = $verif->fetch();
+			if ($verif['verif'] != 0)
+			{
+				echo '<p class="name7" style="font-size: p1.5;">ATTENTION : Vous possédez des sorts de hauts niveaux resultant d\'event ou de recherchers extrêmement poussée et ne doivent pas être échangées commede vulgaires poisson !</p>';
+			}
 			$irank = 8;
 			while ($irank > 0)
 			{
