5ca5867e84381a9141a46f1b85e2344910bfa341
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-05 15:43:18 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -4,17 +4,16 @@ global $db;
 	$sort = 79;
 	$user = 204;
 	
-	$select = $db->prepare('SELECT m.E_magique, m.E_vitale, m.id, ig.incan_id, ig.user_id, il.id AS sort, il.cost
-	FROM incan_get ig
-	RIGHT JOIN members m ON m.id = ig.user_id
-	LEFT JOIN incan_list il ON ig.incan_id = il.id
-	WHERE ig.user_id = ? AND ig.incan_id = ?');
-	$select->execute(array($user, $sort));
-	$line = $select->fetch();
+	$select = $db->prepare('SELECT * FROM members WHERE id = ?');
+	$select->execute(array($user));
+	$select = $select->fetch();
+	$incan = $db->prepare('SELECT * FROM incan_list WHERE id = ?');
+	$incan->execute(array($sort));
+	$incan = $incan->fetch();
 	
-	$pm = $line['E_magique'];
-	$pv = $line['E_vitale'];
-	$cost = $line['cost'];
+	$pm = $select['E_magique'];
+	$pv = $select['E_vitale'];
+	$cost = $incan['cost'];
 	
 	echo $pm, ' ', $pv, ' ', $cost;
 	
