ddd84aa53d2d8188ec4683cfa1bc5702775c3bdb
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-10 21:57:24 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -32,7 +32,7 @@ echo '<h2>Questionnaire</h2>';
    else
 ?>
 <p>Afin de vous permettre de participer à l'évolution du serveur par la construction, il est important pour nous que vous ayez bien saisi les principes de base de la construction dans le cadre d'un jeu de rôle communautaire.</p>
-<form method="POST" action="#">
+<form method="POST" action="index?p=testpage_2">
    <p>
       Je suis autorisé à construire . . .<br />
       <input type="radio" name="buildat" value="partout" id="partout" /> <label for="partout">N'importe où, tant que ça ne se voit pas.</label><br />
