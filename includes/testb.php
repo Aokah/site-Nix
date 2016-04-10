8eff65eb35204d58db42a3b4816fc95c0f61b8a3
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-11 01:58:41 +0200

    Update testb.php

--- includes/testb.php
+++ includes/testb.php
@@ -15,8 +15,12 @@ if ($_SESSION['connected'])
 	$countj = $line0['plus'] - $line2['moins'];
 	$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
 	$counts = $plus - $moins; $hrpavis = $countj + $counts;
+	$select4 = $db->prepare('SELECT buildok FROM members WHERE buildok= 0 AND id = ?');
+	$select4->execute(array($id)); 
    if ($_SESSION['rank'] > 1 OR $hrpavis >= 10)
    {
+   	if ($select->fetch())
+   	{
 echo '<h2>Questionnaire</h2>';
 
    if (isset($_POST['confirm']))
@@ -135,6 +139,11 @@ echo '<h2>Questionnaire</h2>';
 </form>
 
 <?php
+}
+}
+else
+{
+	echo '<p>Vous avez déjà passé ce test, pourquoi donc le refaire ?</p>';
 }
 }
 else
