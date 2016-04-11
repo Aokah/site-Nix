ccba4e7368a9b4ae610d301a180b7af3dfaea964
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-11 13:12:25 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -2,7 +2,9 @@
 {
 
 global $_POST,$_GET, $db;
-
-
+?>
+  <h2>Groupes et Guildes</h2>
+  <p>Ici seront regroupées les informations basiques concernant les guildes et les groupes du site</p>
+<?php
 }
 ?>
