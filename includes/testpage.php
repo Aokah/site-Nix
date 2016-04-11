8fe5823c866a83209de8c290c120bb7f461cd05b
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-11 13:49:55 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -6,14 +6,33 @@ global $_POST,$_GET, $db;
   <h2>Groupes et Guildes</h2>
   <p>Ici seront regroupées les informations basiques concernant les guildes et les groupes du site</p>
   <?php
-  $select = $db->prepare('SELECT gn.id, gn.name, gn.vanish, gn.guild, gm.id, gm.user_id, gm.group_id, gm.user_rank, m.id,
-  m.name, m.rank, m.title
-  FROM group_name gn
-  RIGHT JOIN group_members gm ON gn.id = gm.group_id
-  LEFT JOIN members m ON gm.user_id = m.id
-  WHERE gn.id = ? AND gn.vanish = 0
-  ORDER BY gn.guild DESC, gn.name ASC');
-  $select->execute(array($id));
+  $select = $db->query('SELECT id, name, vanish, guild FROM group_name WHERE vanish = 0 ORDER BY guild DESC, name ASC');
+  
+  while ($line = $select->fetch())
+  {
+    $sel = $db->prepare('SELECT gm.id, gm.user_id, gm.group_id, gm.user_rank, m.id, m.name, m.rank, m.title
+    FROM group_members gm
+    RIGHT JOIN members m ON gm.user_id = m.id
+    WHERE gm.group_id = ?
+    ORDER BY gm.user_rank DESC, m.rank DESC, m.name ASC');
+    $sel->execute(array($line['id']));
+  ?>
+  <img src="pics/guild_<?= $line['id']?>.png" atl="" class="guild" />
+  <ul>
+    <?php
+    while ($sel = $sel->fetch())
+    {
+      if ($sel['rank'] == 9) { $rank = "titan"; } elseif ($sel['rank'] == 10) { $rank = "crea";} else { $rank = $sel['rank'];}
+      ?>
+      <li>
+        <img src="pics/rank<?= $rank?>.png" alt="" class="magie_type" width="25" /> <?= $sel['title'], ' ', $sel['name']?>
+      </li>
+      <?php
+    }
+    ?>
+  </ul>
+  <?php
+  }
   ?>
 <?php
 }
