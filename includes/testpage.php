7d1f8018e33e4519b4560ccdf87af1ab129a1333
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-11 13:25:20 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -5,6 +5,16 @@ global $_POST,$_GET, $db;
 ?>
   <h2>Groupes et Guildes</h2>
   <p>Ici seront regroupées les informations basiques concernant les guildes et les groupes du site</p>
+  <?php
+  $select = $db->prepare('SELECT gn.id, gn.name, gn.vanish, gn.guild, gm.id, gm.user_id, gm.group_id, gm.user_rank, m.id,
+  m.name, m.rank, m.title
+  FROM group_name gn
+  RIGHT JOIN group_members gm ON gn.id = gm.group_id
+  LEFT JOIN members m ON gm.user_id = m.id
+  WHERE gn.id = ? AND gn.vanish = 0
+  ORDER BY gn.guild DESC, gn.name ASC');
+  $select->execute(array($id));
+  ?>
 <?php
 }
 ?>
