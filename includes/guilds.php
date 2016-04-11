0947bc74ab065c093ba754c38d8d4877e66b53e3
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-11 21:37:49 +0200

    Update guilds.php

--- includes/guilds.php
+++ includes/guilds.php
@@ -292,15 +292,11 @@ echo "<h2>Groupes et Guildes</h2>";
   <?php
   }
   
-    $select_ = $db->query('SELECT id, name, vanish, guild FROM group_name WHERE vanish = 1 ORDER BY  name ASC');
-  if ($_SESSION['id'] < 6) {
-  $select2_ = $db->prepare('SELECT gn.name, gn.id, gn.guild, gm.user_id, gm.group_id, gm. user_id, gn.vanish
-  FROM group_name gn 
-  RIGHT JOIN group_members gm ON group_id = gn.id
-  WHERE user_id = ? AND vanish = 1
-  ORDER BY guild DESC, gn.name ASC');
-  $select2_->execute(array($_SESSION['id']));
-  } else { $select2_  = $db->query('SELECT * FROM group_name WHERE vanish = 1 ORDER BY name ASC'); }
+$select2_ = $db->prepare('SELECT gn.id, gn.name, gn.vanish, gn.guild, gm.id,gm.user_id, gm.group_id, gm.user_rank
+FROM group_name gn
+RIGHT JOIN group_members gm ON gm.group_id = gn.id
+WHERE vanish = 1
+ORDER BY gn.guild DESC, gn.name ASC')
   while ($line_ = $select2_->fetch())
   {
     $verif_ = $db->prepare('SELECT * FROM group_members WHERE user_id = ? AND user_rank > 3 AND group_id = ?');
