53ca0b456977edde406b5ed09639001aa3ae519a
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-22 17:23:00 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -3,17 +3,12 @@
 global $_SESSION, $db;
   $select = $db->prepare('SELECT * FROM trophee_get WHERE user_id = ?');
   $select->execute(array($_SESSION['id']));
-  echo $_SESSION['id'];
+  
   while ($line = $select->fetch())
-  { echo $line['trophee_id'];
-    $select2 = $db->prepare('SELECT * FROM trophee_list WHERE id = ?');
-    $select2->execute(array($line['trophee_id']));
-    while ($line2 = $select2->fetch())
-    {
+  {
     ?>
-    <img src="pics/trophee/trophee_<?= $select2['id']?>.png" alt="" width="40%" />
+    <img src="pics/trophee/trophee_<?= $line['trophee_id']?>.png" alt="" width="40%" />
     <?php
-    }
   }
 }
 ?>
