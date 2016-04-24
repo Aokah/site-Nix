5cd84d2d9ca3128e85680557d9413c2c47e82997
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-24 14:42:03 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -1,7 +1,23 @@
 <?php function testpage_3 ()
 {
 ?>
-
+<div>
+  <table cellspacing="0" cellpadding="0" style="background-color:whte;">
+    <tr>
+      <td>
+        En-tête
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Contenu
+      </td>
+    </tr>
+    <tr>
+      Pied de page
+    </tr>
+  </table>
+</div>
 <?php 
 }
 ?>
