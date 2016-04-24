c1023e546d9a204837090f765091d5264ab1dee0
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-24 14:50:21 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -5,9 +5,20 @@
   <table cellspacing="0" cellpadding="0" style="background-color:white;" width="100%">
     <tbody>
       <tr>
-        <td>
-          En-tête
-        </td>
+        <td width="20%">
+			  	<a href="index.php"><img src="pics/logo1.gif" alt="" /></a>
+				</td>
+				<td style="text-align: right;" width="600%">
+					<p>
+						<?php include('includes/news.php'); news('disp');?>
+					</p>
+				</td>
+				<td width="10%">
+					<img src="http://herobrine.fr/blapproved.php?bid=3" title="Approved by BL">
+				</td>
+				<td width="10%x">
+					<a href="https://minecraft.net/" target=_blank><img src="http://herobrine.fr/pics/mc1.png" alt="" /></a>
+				</td>
       </tr>
       <tr>
         <td>
