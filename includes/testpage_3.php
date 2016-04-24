1f5842b49097d7b07eba027173f51b2ba0ac220f
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-24 15:40:24 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -5,34 +5,46 @@
   <table cellspacing="0" cellpadding="0" style="background-color:white;" width="100%">
     <tbody>
       <tr>
-        <td width="20%">
-		<a href="index.php"><img src="pics/logo1.gif" alt="" /></a>
-	</td>
-	<td style="text-align: right;" width="600%">
-		<p>
-			News
-		</p>
-	</td>
-	<td width="10%">
-		<img src="http://herobrine.fr/blapproved.php?bid=3" title="Approved by BL">
-	</td>
-	<td width="10%">
-		<a href="https://minecraft.net/" target=_blank><img src="http://herobrine.fr/pics/mc1.png" alt="" /></a>
-	</td>
+      	<td>
+      		<table cellspacing="0" cellpadding="0">
+      			<tbody>
+      				<td width="20%">
+					<a href="index.php"><img src="pics/logo1.gif" alt="" /></a>
+				</td>
+				<td style="text-align: right;" width="600%">
+					<p>
+						News
+					</p>
+				</td>
+				<td width="10%">
+					<img src="http://herobrine.fr/blapproved.php?bid=3" title="Approved by BL">
+				</td>
+				<td width="10%">
+					<a href="https://minecraft.net/" target=_blank><img src="http://herobrine.fr/pics/mc1.png" alt="" /></a>
+				</td>
+      			</tbody>
+      		</table>
+      	</td>
       </tr>
       <tr>
-        <td>
-        	Menu A
-        </td>
-        <td>
-        	menu B
-        </td>
-        <td>
-        	menu C
-        </td>
-        <td>
-        	menu D
-        </td>
+      	<table cellspacing="0" cellpadding="0" style="background-color:white;" width="100%">
+      		<tbody>
+      			<tr>
+      				<td>
+			        	Menu A
+			        </td>
+			        <td>
+			        	menu B
+			        </td>
+			        <td>
+			        	menu C
+			        </td>
+			        <td>
+			        	menu D
+			        </td>
+      			</tr>
+      		</tbody>
+      	</table>
       </tr>
       <tr>
         <td>
