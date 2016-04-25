5e1c71e0a2b17ed6d88159aafe11a444aa9d3a10
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-25 17:49:10 +0200

    Update aside.php

--- includes/aside.php
+++ includes/aside.php
@@ -1,7 +1,5 @@
 <?php function aside()
 {
-<?if ($_SESSION["rank"] >= 5) { if ($line['count'] >= 1) {?>
-							<span style="color: red">[<?= $line['count']?>]</span>
-						<? } } ?>
+	include ('includes/loggedIn.php');	loggedIn();
 }
 ?>
