af1152c2467eb1108415010eb2b26b37476347c2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-27 14:33:30 +0200

    Update home.php

--- includes/home.php
+++ includes/home.php
@@ -2,29 +2,32 @@
 { 
 ?>
 	
-		<h2>Bienvenue sur <strong>Nix</strong>, étranger !</h2>
+		<h2>Bienvenue sur Nix, étranger !</h2>
 
 	<h3>Mais qu'est-ce donc ?</h3>
-	<p>Nix est un serveur à vocation rôliste autour d'un univers nordique se divisant entre la politique des institutions et une étude de certaines choses étranges que les habitants ont nommé communément "la <em>magie</em>".
-	RPNix.com vous propose une aventure dans l'imaginaire collectif. Ce dernier s'adresse aux joueurs de <em>Minecraft</em> souhaitant parfaire leur experience de jeu en ajoutant des éléments scénaristiques uniques !</p>
+	<p>Nix est un serveur à vocation rôliste autour d'un univers peu commun se divisant entre la politique, ainsi que des études diverse sur des choses étranges que les habitants ont nommé communément "la magie". 
+	RPNix.com vous propose une aventure dans l'imaginaire collectif. Ce dernier s'adresse aux joueurs de Minecraft souhaitant parfaire leur expérience de jeu en ajoutant des éléments scénaristiques uniques ! </p>
 		
 	<h3>Mais concrètement, comment ça marche ?</h3>
-	<p>Il vous suffit de vous inscrire et de créer votre personnage pour commencer cette grande <em>aventure</em> au sein de notre communauté.<br />
-	A votre arrivez vous incarnez un "itinérant" qui après un certain voyage s'est abrité dans une maison histoire de s'abriter du froid et de se retaurer, après un temps de repos vous allez pouvoir prendre contact avec les différentes
-	Places Fortes de la contrée de <strong>Nix</strong> afin de faire d'évoluer au sein de l'échelle sociale locale.<br />
-	RPNix.com est un meta-jeu, vous proposant différentes choses, allant de la maintenant de l'ordre au sein de votre institution jusqu'à sa direction, libre d'ajouter à votre institution votre encre à sa grande histoire !</p>
+	<p>Il vous suffit de vous inscrire et de créer votre personnage pour commencer cette grande aventure au sein de notre communauté.<br />
+	A votre arrivez vous incarnez un voyageur qui après une longue marche arrive sur des terres enneigé. Votre personnage pourra donc très vite prendre contact avec les différentes personnes de Nix.  
+	Ainsi se rendre compte que la région de Nix est très peu commune, Elle semble abriter la dernière communauté magique qui veille encore sur ce Monde. RPNix.com est un meta-jeu, vous proposant différentes choses. 
+	Votre personnage pourra ainsi évolué dans ce monde et peut être marqué l'histoire de Nix. Allant du simple débutant parmi tant d'autres au plus puissant des Sages de l'histoire.  Cependant Nix ne vous proposes pas que de la magie. 
+	En effet certaine personnes ne sont pas très enjoué par celle-ci et favoriserons donc la technologie !<br />
+	Rejoignez cette grande aventure au plus vite !/p>
 	
-	<img src="pics/imghome.png" alt="Image d'Accueil"/>
+	<img src="pics/imghome.png" alt=" "/>
 	
 	<h3>Comment puis-je progresser sur ce serveur ?</h3>
-	RPNix.com vous propose deux schéma de progresser bien distincts :<br />
-	D'une part la progression <em>RP</em>, notée par le grade de votre personnage au sein de son institution visible sur la page qui regroupe les différentes institutions et d'autre part la progression dit "HRP" du joueur à travers les grades du site qui lu permetteront
-	à terme d'encadrer les autres voire même d'ajouter du contenu auprès des autres Maîtres du Jeu !</p>
+	RPNix.com vous propose deux schémas de progresser bien distincts :<br />
+	D'une part la progression RP, notée par le grade de votre personnage au sein de son institution visible sur la page qui regroupe les différentes institutions et d'autre part la progression dit "HRP" du joueur à travers les grades du site qui lu permettront 
+	à terme d'encadrer les autres voire même d'ajouter du contenu auprès des autres Maîtres du Jeu !
+</p>
 	
-	<img src="pics/grades.png" alt="Progression HRP" title="Grades HRP"/>
+	<img src="pics/grades.png" alt=" " title="Grades HRP"/>
 	<h3>Votre personnage, votre histoire !</h3>
-	<p>Chaque personnage possède une personalité et un passé qui lui est propre, il ne tient qu'à vous d'apporter votre pierre à l'édifice historique de notre serveur.<br />
-	Seul l'avenir sait si vous deviendrez un grand <strong>Mage</strong>, un Chevalier de renom, un Diplomate de génie ou un Membre investi dans sa comunauté !</p>
+	<p>Chaque personnage possède une personnalité et un passé qui lui est propre, il ne tient qu'à vous d'apporter votre pierre à l'édifice historique de notre serveur.<br />
+	Seul l'avenir sait si vous deviendrez un grand Mage, un Chevalier de renommé, un Diplomate de génie ou un Membre investi dans sa communauté !</p>
 	
 	
 <?php	
