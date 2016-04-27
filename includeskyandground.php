9d83f9b55e921a7c277fcb0f75679b2b7b275b1b
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-27 11:32:45 +0200

    Create includeskyandground.php

---
+++ includeskyandground.php
@@ -0,0 +1,20 @@
+<?php
+	{
+	?>
+		<!DOCTYPE html>
+			<html>
+				<head>
+					<meta charset="UTF-8" />
+					<title>Herobrine Sky &amp; Ground</title>
+					<link href="includes/style.css" />
+				</head>
+
+				<body>
+							
+					<?php include ('includes/pages.php'); page();
+		
+				</body>
+			</html>
+	<?php
+	}
+?>
