21f87cc1eef15e781671ab4b961eabe288ec1022
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-04-28 22:12:33 +0200

    Update sondage.php

--- includes/sondage.php
+++ includes/sondage.php
@@ -386,9 +386,9 @@
 				{	
 					$verr = ($line['verr'] == 1) ? '[Verrouillé] ' : '';
 				$date = preg_replace('#^(.{4})-(.{2})-(.{2}) (.{2}:.{2}):.{2}$#', 'Le $3/$2/$1 à $4', $line['date_post']);
-				$verify = $db->prepare('SELECT * FROM sondage_unread WHERE unread = 1 AND sondage_id = ? AND user_id = ?');
-				$verify->execute(array($line['id'], $_SESSION['id']));
-				$read = ($verif = $verify->fetch()) ? 'read' : 'unread';
+				$verify = $db->prepare('SELECT * FROM sondage_unread WHERE unread = 1 AND sondage_id = 5 AND user_id = ?');
+				$verify->execute(array($_SESSION['id']));
+				$read = ($verify->fetch()) ? 'read' : 'unread';
 				?>
 				<tr class="<?= $read?>">
 					<td>
