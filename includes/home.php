6ee338d5624d07194f449fa159e602e3bbcc3408
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-02 10:45:20 +0200

    Update home.php

--- includes/home.php
+++ includes/home.php
@@ -5,22 +5,22 @@
 		<h2>Bienvenue sur Nix, étranger !</h2>
 
 	<h3>Mais qu'est-ce donc ?</h3>
-	<p>Nix est un serveur à vocation rôliste autour d'un univers peu commun se divisant entre la politique, ainsi que des études diverse sur des choses étranges que les habitants ont nommé communément "la magie". 
-	RPNix.com vous propose une aventure dans l'imaginaire collectif. Ce dernier s'adresse aux joueurs de Minecraft souhaitant parfaire leur expérience de jeu en ajoutant des éléments scénaristiques uniques ! </p>
+	<p>Nix est un serveur à vocation rôliste autour d'un univers peu commun se divisant entre la politique, ainsi que des études diverses sur des choses étranges que les habitants ont nommé communément "la Magie". 
+	RPNix.com vous propose une aventure dans l'imaginaire collectif. Ce dernier s'adressant aux joueurs de Minecraft souhaitant parfaire leur expérience de jeu en y ajoutant des éléments scénaristiques uniques ! </p>
 		
 	<h3>Mais concrètement, comment ça marche ?</h3>
 	<p>Il vous suffit de vous inscrire et de créer votre personnage pour commencer cette grande aventure au sein de notre communauté.<br />
-	A votre arrivez vous incarnez un voyageur qui après une longue marche arrive sur des terres enneigé. Votre personnage pourra donc très vite prendre contact avec les différentes personnes de Nix.  
-	Ainsi se rendre compte que la région de Nix est très peu commune, Elle semble abriter la dernière communauté magique qui veille encore sur ce Monde. RPNix.com est un meta-jeu, vous proposant différentes choses. 
-	Votre personnage pourra ainsi évolué dans ce monde et peut être marqué l'histoire de Nix. Allant du simple débutant parmi tant d'autres au plus puissant des Sages de l'histoire.  Cependant Nix ne vous proposes pas que de la magie. 
-	En effet certaine personnes ne sont pas très enjoué par celle-ci et favoriserons donc la technologie !<br />
+	A votre arrivée, vous incarnez un voyageur, qui, après une longue marche arrive sur des terres enneigé. Votre personnage pourra donc très vite prendre contact avec les différentes personnes de Nix.  
+	Ainsi se rendre compte que la région de Nix est très peu commune, elle semble abriter la dernière communauté magique qui veille encore sur ce Monde. RPNix.com est un meta-jeu, vous proposant différentes choses. 
+	Votre personnage pourra ainsi évoluer dans ce monde et peut être marquer son histoire. Allant du simple débutant parmi tant d'autres au plus puissant des Sages de tous les temps.  Cependant Nix ne vous proposes pas que de la magie. 
+	En effet certaine personnes ne sont pas très enjouées par celle-ci et favoriseront donc la technologie !<br />
 	Rejoignez cette grande aventure au plus vite !</p>
 	<div style="text-align:center;">
 		<img src="pics/imghome.png" alt=" " width="70%"/>
 	</div>
 	<h3>Comment puis-je progresser sur ce serveur ?</h3>
-	RPNix.com vous propose deux schémas de progresser bien distincts :<br />
-	D'une part la progression RP, notée par le grade de votre personnage au sein de son institution visible sur la page qui regroupe les différentes institutions et d'autre part la progression dit "HRP" du joueur à travers les grades du site qui lu permettront 
+	RPNix.com vous propose deux schémas de progression bien distincts :<br />
+	D'une part la progression RP, notée par le grade de votre personnage au sein de son institution visible sur la page qui les regroupe et d'autre part la progression dite "HRP" du joueur à travers les grades du site qui lui permettront 
 	à terme d'encadrer les autres voire même d'ajouter du contenu auprès des autres Maîtres du Jeu !
 </p>
 	<table width="100%" style="text-align:center;">
