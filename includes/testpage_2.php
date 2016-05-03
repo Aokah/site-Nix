3b19e65eab013bec43b4e196a38912d845fbbb6d
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 15:03:56 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,8 +1,18 @@
 <?php function testpage_2 ()
 {
 global $_SESSION, $db;
-$psw = $db->prepare('SELECT password, id FROM members WHERE id = ?'); $psw->execute(array($_SESSION['id'])); $line = $psw->fetch();
+$psw = $db->prepare('SELECT password, id FROM members WHERE id = ?'); $psw->execute(array($_SESSION['id'])); 
 
-echo (password_hash('Dragonball76',PASSWORD_DEFAULT));
+if ($line = $psw->fetch())
+{
+  if (password_verify('dragonball76', $line['password']))
+  {
+    echo 'yes';
+  }
+  else
+  {
+    echo 'no';
+  }
+}
 }
 ?>
