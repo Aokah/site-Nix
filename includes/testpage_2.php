10fed619680542b2975ceafe6a27699468af6763
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 15:01:13 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -2,13 +2,7 @@
 {
 global $_SESSION, $db;
 $psw = $db->prepare('SELECT password, id FROM members WHERE id = ?'); $psw->execute(array($_SESSION['id'])); $line = $psw->fetch();
-  if (password_verify('Dragonball76',$line['password']))
-  {
-    echo 'MDP correct';
-  }
-  else
-  {
-    echo 'Mauvais MDP';
-  }
+
+echo (password_hash('Dragonball76'));
 }
 ?>
