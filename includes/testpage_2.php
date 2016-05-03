e5a7dd4052a4a02a8e2735b05e01e34c68e5ecb2
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 14:55:47 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,10 +1,13 @@
 <?php function testpage_2 ()
 {
 global $_SESSION, $db;
-  $psw = $db->prepare('SELECT * FROM members WHERE password = ?'); $psw->execute(array('$2y$10$nE18H5lNZQJDF0Abn3bak.fTLd.3X0reabnkNCY9XNZZo0M4rLp7O'));
-  while ($line = $psw->fetch())
+  if (password_verify('Dragonball76',$_SESSION['name']);
   {
-    echo $line['name'], ', ';
+    MDP correct
+  }
+  else
+  {
+    Mauvais MDP
   }
 }
 ?>
