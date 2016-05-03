b434ee1bb65258974325dd3cf2b2e0c1255eeaed
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 14:56:53 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,7 +1,7 @@
 <?php function testpage_2 ()
 {
 global $_SESSION, $db;
-  if (password_verify('Dragonball76',$_SESSION['name'])
+  if (password_verify('Dragonball76',$_SESSION['name']))
   {
     echo 'MDP correct';
   }
