73a9064ccc8ad67440ab3467cfac5ae5365fd2d8
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 14:50:27 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,7 +1,7 @@
 <?php function testpage_2 ()
 {
 global $_SESSION, $db;
-  $psw = $db->query('SELECT password, name FROM members WHERE password = Dragonball76');
+  $psw = $db->query('SELECT password, name FROM members WHERE password = \"Dragonball76\"');
   while ($line = $psw->fetch())
   {
     echo $line['name'], ', ';
