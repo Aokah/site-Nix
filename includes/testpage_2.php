2f285b7832b2fc17c95c710711981882b2b28b4c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-03 15:01:41 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -3,6 +3,6 @@
 global $_SESSION, $db;
 $psw = $db->prepare('SELECT password, id FROM members WHERE id = ?'); $psw->execute(array($_SESSION['id'])); $line = $psw->fetch();
 
-echo (password_hash('Dragonball76'));
+echo (password_hash('Dragonball76',PASSWORD_DEFAULT));
 }
 ?>
