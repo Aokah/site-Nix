cdfd6caaa9883dc510dde36f0ca7520264e3d33a
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-04 14:52:26 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -17,8 +17,43 @@
     <p>Ici sont répertoriées les différentes candidatures d'entrée à Nix n'étant pas encore validée.</p>
     <? if ($select['count'] > 0)
     {
+      $select = $db->query('SELECT c.id AS c_id, c.sender_id, c.pseudo_mc, c.candid, c.date_send, c.verify, m.id, m.title, m.name
+      FROM candid c
+      RIGHT JOIN members m ON m.id = c.sender_id
+      WHERE verify = 0 ORDER BY date_send DESC');
     ?>
     <p>Il y a actuellement <?=$select['count'], ' candidature', $plural; ?> en attente de lecture.</p>
+    
+    <table cellspacing="0" cellpadding="0">
+      <tbody>
+        <th>Joueur</th>
+        <th>Date d'envoi</th>
+        <th>Pseudo MC</th>
+        <th>Candidature</th>
+        <? while ($line = $select->fetch())
+        {
+        $date = preg_replace('#^(.{4})-(.{2})-(.{2}) (.{2}:.{2}):.{2}$#', '$3/$2/$1 à $4', $line['date_send']);
+        ?>
+        <tr>
+          <td>
+            <?= $line['name']?>
+          </td>
+          <td>
+            <?= $date ?>
+          </td>
+          <td>
+            <?= $line['pseudo_mc']?>
+          </td>
+          <td>
+            <?= $line['candidature']?>
+          </td>
+        </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+    
     <?
     }
     else
