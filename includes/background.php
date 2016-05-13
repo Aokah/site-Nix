c616a058c6f9fda0212d3cfd9c25e43f960ff53f
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-13 22:17:40 +0200

    Update and rename testpage.php to background.php

---
+++ includes/background.php
@@ -0,0 +1,66 @@
+<?php function testpage()
+{
+  if ($_SESSION['connected'])
+  {
+    if ($_SESSION['rank'] > 4)
+    {
+      if (isset($_GET['type']))
+      {
+        // Affichage des sujets
+        if (isset($_GET['sub']))
+        {
+          //Affichage des  éléments
+          if (isset($_GET['id']))
+          {
+            // Détails du BG
+          }
+          else
+          {
+            // BGs
+          }
+        }
+        else
+        {
+          //Liste des sous-type
+        }
+      }
+      else
+      {
+       $select = $db->prepare('SELECT * FROM bg_type WHERE level >= ?'); $select->execute(array($_SESSION['rank']));
+       ?>
+          <table cellspacing="5" cellpadding="10">
+            <tbody>
+              <tr>
+                <th colspan="2">Sujets Généraux</th>
+                <th>Niveau de visionnage</th>
+              </tr>
+        <?php
+        while ($line = $select->fetch())
+        {
+          switch ($line['level']) 
+          {
+            case 5: $level = "Modérateur"; break; case 6: $level = "Maître du Jeu"; break; case 7: $level = "Opérateur"; break;
+          }
+          ?>
+              <tr>
+                <td><img src="pics/ico/bg_type_<? $line['id']?>" alt="" /></td>
+                <td><a href="index?p=background&type=<?= $line['id']?>"><?= $line['type']?></a></td>
+                <td><?= $level?></td>
+              </tr>
+            </tbody>
+          </table>
+          <?php
+        }
+      }
+    }
+    else
+    {
+      echo '<p>Vous n\'avez pas le niveau requis pour accéder à cette page.</p>';
+    }
+  }
+  else
+  {
+    echo '<p>Connectez-vous pour accéder à cette page.</p>';
+  }
+}
+?>
