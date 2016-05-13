ab22ac337b4cf533b3949baeaf0e2e270d5a335c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-13 23:40:07 +0200

    Update background.php

--- includes/background.php
+++ includes/background.php
@@ -38,12 +38,57 @@
       }
       elseif (isset($_GET['sub']))
       {
-          $sub = intval($_GET['sub']);
-          $verify = $db->prepare('SELECT * FROM bg_sub WHERE id = ?'); $verify->execute(array($sub));
+        $sub = intval($_GET['sub']);
+        ?>
+        <table cellspacing="5" cellpadding="10">
+            <tbody>
+              <tr>
+                <th colspan="2">Types de sujets</th>
+                <th>Niveau de visionnage</th>
+              </tr>
+        <?php
+        $select = $db->prepare('SELECT * FROM bg_sub WHERE sub_id = ? AND level <= ?'); $select->execute(array($sub, $_SESSION['rank']));
+        while ($line = $select->fetch())
+        {
+          switch ($line['level']) 
+          {
+            case 5: $level = "Modérateur"; break; case 6: $level = "Maître du Jeu"; break; case 7: $level = "Opérateur"; break;
+          }
+        ?>
+              <tr>
+                <td><img src="pics/ico/bg_id_<?= $line['id']?>" alt="" /></td>
+                <td><a href="index?p=background&id=<?= $line['id']?>"><?= $line['subject']?></a></td>
+                <td><?= $level?></td>
+              </tr>
+        <?php
+        }
+        ?>
+            </tbody>
+          </table>
+        <?php
       }
-      elseif
+      elseif (isset($_GET['id']))
       {
-        
+        $id = intval($_GET['id']);
+        $select = $db->prepare('SELECT * FROM bg_id WHERE id = ?'); $selec->execute(array($id));
+        if ($line = $select->fetch())
+        {
+          if ($line['level'] <= $_SESSION['rank'])
+          {
+        ?>
+        <h3><?= $line['title']?></h3>
+        <p><?= $line['content']?></p>
+        <?php
+          }
+          else
+          {
+            echo '<p>Essaieraiu-tu de tricher et de regarder du contenu qui n\'est pas de ton niveau ? :p</p>';
+          }
+        }
+        else
+        {
+          echo '<p>Désolé mais vous avez séléctionné un ID inexistant.</p>';
+        }
       }
       else
       {
