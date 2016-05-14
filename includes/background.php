500da7eeb2ff3e454e0dee75d11d4f2ace4cbc43
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-14 20:48:28 +0200

    Update background.php

--- includes/background.php
+++ includes/background.php
@@ -108,8 +108,8 @@
           <table cellspacing="5" cellpadding="10" align="center" width="50%">
             <tbody>
               <tr>
-                <th  class="bg" colspan="2">Sujets Généraux</th>
-                <th  class="bg">Niveau de visionnage</th>
+                <th  class="bgth" colspan="2">Sujets Généraux</th>
+                <th  class="bgth">Niveau de visionnage</th>
               </tr>
         <?php
         while ($line = $select->fetch())
@@ -120,9 +120,9 @@
           }
           ?>
               <tr>
-                <td  class="bg" style="padding:3%"><img src="pics/ico/bg_type_<?= $line['id']?>" alt="" /></td>
-                <td class="bg"><a href="index?p=background&type=<?= $line['id']?>"><div width="100%" style="padding:3%"><?= $line['type']?></div></a></td>
-                <td  class="bg" style="padding:3%"><?= $level?></td>
+                <td  class="bgtd" style="padding:3%"><img src="pics/ico/bg_type_<?= $line['id']?>" alt="" /></td>
+                <td class="bgtd"><a href="index?p=background&type=<?= $line['id']?>"><div width="100%" style="padding:3%"><?= $line['type']?></div></a></td>
+                <td  class="bgtd" style="padding:3%"><?= $level?></td>
               </tr>
           <?php
         }
