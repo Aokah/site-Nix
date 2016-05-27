2e3e40db29e221910ec6ff9353bb342561640243
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-27 23:49:39 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -12,7 +12,7 @@
           case "Air" : $type = 1; break; case "Arcane": $type = 2; break; case "Chaos": $type = 3; break; case "Eau": $type = 4; break;
           case "Energie": $type = 5; break; case "Espace": $type = 6; break; case "Feu": $type = 7; break; case "Glace": $type = 8; break;
           case "Métal": $type = 9; break; case "Nature": $type = 10; break; case "Ombre": $type = 11; break; case "Ordre": $type = 12; break;
-          case "Psy": $type = 13; break; case "Températures": $type = 14; break; case "Terre": $type = 15; break; case "Void": $type = 16; break;
+          case "Psy": $type = 13; break; case "Chaleur": $type = 14; break; case "Terre": $type = 15; break; case "Void": $type = 16; break;
         }
         $select = $db->prepare('SELECT * FROM skil_list WHERE type = ? ORDER BY number ASC'); $select->execute(array($type));
         if ($_GET['element'] == "Air")
