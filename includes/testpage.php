0b6be5c789aeb9190fb7520ba24482a0ab2ff956
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-27 22:56:35 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -1,7 +1,41 @@
 <?php function testpage()
 {
-?>
-
-<?php
+  global $db, $_SESSION, $_POST, $_GET;
+  
+  $select = $db->prepare('SELECT id, specialisation, spe_2 FROM members WHERE id = ?'); $select->execute(array($_SESSION['id']));
+  $select = $select->fetch();
+  
+      if (isset($_GET['element']))
+      {
+        switch ($_GET['element'])
+        {
+          case "Air" : $type = 1; break; case "Arcane": $type = 2; break; case "Chaos": $type = 3; break; case "Eau": $type = 4; break;
+          case "Energie": $type = 5; break; case "Espace": $type = 6; break; case "Feu": $type = 7; break; case "Glace": $type = 8; break;
+          case "Métal": $type = 9; break; case "Nature": $type = 10; break; case "Ombre": $type = 11; break; case "Ordre": $type = 12; break;
+          case "Psy": $type = 13; break; case "Températures": $type = 14; break; case "Terre": $type = 15; break; case "Void": $type = 16; break;
+        }
+        $select = $db->prepare('SELECT * FROM skil_list WHERE type = ?'); $select->execute(array($type));
+        if ($_GET['element'] == "Air")
+        {
+          ?>
+          <table cellspacing="0" cellpadding="0" class="">
+            <tbody>
+              <tr>
+                <th> </th>
+                <th>Compétence</th>
+              </tr>
+              <tr>
+                <td><?= $cost?></td>
+                <td><?= $line['name']?></td>
+              </tr>
+            </tbody>
+          </table>
+          <?php
+        }
+      }
+      else
+      {
+        //Selection
+      }
 }
 ?>
