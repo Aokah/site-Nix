f62ac917317f7c9eb40231b2c4f24000596edbd7
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-28 01:15:36 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -4,20 +4,34 @@
   
   $select = $db->prepare('SELECT id, specialisation, spe_2 FROM members WHERE id = ?'); $select->execute(array($_SESSION['id']));
   $select = $select->fetch();
+  $width1 = ($_GET['element'] == "Air")? '60' : '40';
+  $width2 = ($_GET['element'] == "Arcane")? '60' : '40';
+  $width3 = ($_GET['element'] == "Chaos")? '60' : '40';
+  $width4 = ($_GET['element'] == "Eau")? '60' : '40';
+  $width5 = ($_GET['element'] == "Energie")? '60' : '40';
+  $width6 = ($_GET['element'] == "Feu")? '60' : '40';
+  $width7 = ($_GET['element'] == "Glace")? '60' : '40';
+  $width8 = ($_GET['element'] == "Lumiere")? '60' : '40';
+  $width9 = ($_GET['element'] == "Metal")? '60' : '40';
+  $width10 = ($_GET['element'] == "Nature")? '60' : '40';
+  $width11 = ($_GET['element'] == "Ombre")? '60' : '40';
+  $width12 = ($_GET['element'] == "Psy")? '60' : '40';
+  $width13 = ($_GET['element'] == "Terre")? '60' : '40';
+  
   ?>
-  <a href="index.php?p=testpage&element=Air"><img src="pics/ico/Magie_Air.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de l'Air" alt="" /></a>
-	<? if ($select['specialisation'] == "Arcane" OR $select['spe_2'] == "Arcane" OR $_SESSION['rank'] > 5) {?>	<a href="index.php?p=testpage&element=Arcane"><img src="pics/ico/Magie_Arcane.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de l'Arcane" alt="" /></a><? } ?>
-	<? if ($select['specialisation'] == "Chaos" OR $select['spe_2'] == "Chaos" OR $_SESSION['rank'] > 5) {?>	<a href="index.php?p=testpage&element=Chaos"><img src="pics/ico/Magie_Chaos.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie du Chaos" alt="" /></a><? } ?>
-		<a href="index.php?p=testpage&element=Eau"><img src="pics/ico/Magie_Eau.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de l'Eau" alt="" /></a>
-		<a href="index.php?p=testpage&element=Energie"><img src="pics/ico/Magie_Energie.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de l'Energie" alt="" /></a>
-		<a href="index.php?p=testpage&element=Feu"><img src="pics/ico/Magie_Feu.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie du Feu" alt="" /></a>
-		<a href="index.php?p=testpage&element=Glace"><img src="pics/ico/Magie_Glace.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de la Glace" alt="" /></a>
-		<a href="index.php?p=testpage&element=Lumiere"><img src="pics/ico/Magie_Lumiere.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de la Lumière" alt="" /></a>
-		<a href="index.php?p=testpage&element=Metal"><img src="pics/ico/Magie_Metal.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie du Metal" alt="" /></a>
-		<a href="index.php?p=testpage&element=Nature"><img src="pics/ico/Magie_Nature.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de la Nature" alt="" /></a>
-		<a href="index.php?p=testpage&element=Ombre"><img src="pics/ico/Magie_Ombre.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de l'Ombre" alt="" /></a>
-		<a href="index.php?p=testpage&element=Psy"><img src="pics/ico/Magie_Psy.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie du Psy" alt="" /></a>
-		<a href="index.php?p=testpage&element=Terre"><img src="pics/ico/Magie_Terre.png" width="<?= $width?>" class="magie_type" title="Selectionner la magie de la Terre" alt="" /></a>
+  <a href="index.php?p=testpage&element=Air"><img src="pics/ico/Magie_Air.png" width="<?= $width1?>" class="magie_type" title="Selectionner la magie de l'Air" alt="" /></a>
+	<? if ($select['specialisation'] == "Arcane" OR $select['spe_2'] == "Arcane" OR $_SESSION['rank'] > 5) {?>	<a href="index.php?p=testpage&element=Arcane"><img src="pics/ico/Magie_Arcane.png" width="<?= $width2?>" class="magie_type" title="Selectionner l'Essence de l'Arcane" alt="" /></a><? } ?>
+	<? if ($select['specialisation'] == "Chaos" OR $select['spe_2'] == "Chaos" OR $_SESSION['rank'] > 5) {?>	<a href="index.php?p=testpage&element=Chaos"><img src="pics/ico/Magie_Chaos.png" width="<?= $width3?>" class="magie_type" title="Selectionner l'Essence du Chaos" alt="" /></a><? } ?>
+		<a href="index.php?p=testpage&element=Eau"><img src="pics/ico/Magie_Eau.png" width="<?= $width4?>" class="magie_type" title="Selectionner la magie de l'Eau" alt="" /></a>
+		<a href="index.php?p=testpage&element=Energie"><img src="pics/ico/Magie_Energie.png" width="<?= $width5?>" class="magie_type" title="Selectionner la magie de l'Energie" alt="" /></a>
+		<a href="index.php?p=testpage&element=Feu"><img src="pics/ico/Magie_Feu.png" width="<?= $width6?>" class="magie_type" title="Selectionner la magie du Feu" alt="" /></a>
+		<a href="index.php?p=testpage&element=Glace"><img src="pics/ico/Magie_Glace.png" width="<?= $width7?>" class="magie_type" title="Selectionner la magie de la Glace" alt="" /></a>
+		<a href="index.php?p=testpage&element=Lumiere"><img src="pics/ico/Magie_Lumiere.png" width="<?= $width8?>" class="magie_type" title="Selectionner la magie de la Lumière" alt="" /></a>
+		<a href="index.php?p=testpage&element=Metal"><img src="pics/ico/Magie_Metal.png" width="<?= $width9?>" class="magie_type" title="Selectionner la magie du Metal" alt="" /></a>
+		<a href="index.php?p=testpage&element=Nature"><img src="pics/ico/Magie_Nature.png" width="<?= $width10?>" class="magie_type" title="Selectionner la magie de la Nature" alt="" /></a>
+		<a href="index.php?p=testpage&element=Ombre"><img src="pics/ico/Magie_Ombre.png" width="<?= $width11?>" class="magie_type" title="Selectionner la magie de l'Ombre" alt="" /></a>
+		<a href="index.php?p=testpage&element=Psy"><img src="pics/ico/Magie_Psy.png" width="<?= $width12?>" class="magie_type" title="Selectionner la magie du Psy" alt="" /></a>
+		<a href="index.php?p=testpage&element=Terre"><img src="pics/ico/Magie_Terre.png" width="<?= $width13?>" class="magie_type" title="Selectionner la magie de la Terre" alt="" /></a>
   <?php
   
       if (isset($_GET['element']))
