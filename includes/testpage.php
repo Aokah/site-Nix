5e207440bfc5c5ad0de547ab481c85cacb31c683
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-05-29 00:17:33 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -3,7 +3,11 @@
   $limit = 17;
   while ($limit > 0)
   {
-    echo $limit;
+    $select = $db->prepare('SELECT g.id g_id, user_id, skil_id, l.id, l.name, l.cost, l.type, l.infos, l.number
+    FROM skil_get g
+    RIGHT JOIN skil_list l ON g.skil_id = l.id
+    WHERE user_id= ? AND type = ?
+    ORDER BY number ASC');
     $limit --;
   }
 }
