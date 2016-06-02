5fb2844465f4b334b39e7e533eb73cb60340be42
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-02 11:16:53 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -6,7 +6,7 @@
   {
     $verif = $db->prepare('SELECT id, testm FROM members WHERE id = ? AND testm = 0');
     $verif->execute(array($_SESSION['id']));
-    echo '<h2>Test de Personalité Magique</h2>';
+    echo '<h3>Test de Personalité Magique</h3>';
     if ($verif->fetch())
     {
       if (isset($_POST['valid']))
