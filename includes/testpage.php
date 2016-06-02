07f12cd9d194176ed8fb00ef9833303e6484c19f
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-02 20:08:29 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -14,13 +14,13 @@
         $air = 0; $arcane = 0;  $chaleur = 0; $chaos = 0; $eau = 0; $espace = 0; $energie = 0;
         $feu = 0; $glace = 0; $lumiere = 0; $metal = 0;  $nature = 0;  $ombre = 0;  $ordre = 0;
         $psy = 0; $terre = 0;  $void = 0;
-        if ($_POST['Q1'] == "R1") { $ombre++; $void++; } elseif ($_POST['Q1'] == "R2") {$glace++; $eau++; } 
+        if ($_POST['Q1'] == "R1") { $ombre++; } elseif ($_POST['Q1'] == "R2") {$glace++; $eau++; } 
         elseif ($_POST['Q1'] == "R3") {$feu++; $chaos++; } elseif ($_POST['Q1'] == "R4") { $void++; $psy++; $espace++; }
-        elseif ($_POST['Q1'] == "R5") { $nature++; } elseif ($_POST['Q1'] == "R6") {$lumiere++; $energie++; $arcane++; $air++; }
-        elseif ($_POST['Q1'] == "R7") {$chaleur++; $feu++;  } elseif ($_POST['Q1'] == "R8") {$lumiere++; $ordre++;}
+        elseif ($_POST['Q1'] == "R5") { $nature++; } elseif ($_POST['Q1'] == "R6") {$energie++; $arcane++; }
+        elseif ($_POST['Q1'] == "R7") {$chaleur++; } elseif ($_POST['Q1'] == "R8") {$lumiere++; $ordre++;}
         elseif ($_POST['Q1'] == "R9") {$terre++;} elseif ($_POST['Q1'] == "R10") {$metal++; $air++;}
         
-      # Ombre = 1  Void = 2  Glace = 1  Eau = 1  Feu = 2  Chaos = 1  Psy = 1  Nature = 1  Air = 2  Arcane = 1  Lumière = 2 
+      # Ombre = 1  Void = 1  Glace = 1  Eau = 1  Feu = 1  Chaos = 1  Psy = 1  Nature = 1  Air = 1 Arcane = 1  Lumière = 1 
       # Chaleur = 1  Ordre = 1 Terre = 1  Energie = 1  Espace = 1  Metal = 1
       }
       else
@@ -31,17 +31,19 @@
         
         <form method="POST" action="index?p=testpage">
           <p>Quelle couleur pour représente le plus ?</p>
-          <input type="radio" name="Q1" value="R1" id="R1" /> <label for="R1" class="name1">Le Noir</label><br />
-          <input type="radio" name="Q1" value="R2" id="R2" /> <label for="R2" class="name1" style="color:aqua;">Le Bleu</label><br />
-          <input type="radio" name="Q1" value="R3" id="R3" /> <label for="R3" class="name1" style="color:red;">Le Rouge</label><br />
-          <input type="radio" name="Q1" value="R4" id="R4" /> <label for="R4" class="name1" style="color:purple;">Le Violet</label><br />
-          <input type="radio" name="Q1" value="R5" id="R5" /> <label for="R5" class="name1" style="color:lime;">Le Vert</label><br />
-          <input type="radio" name="Q1" value="R6" id="R6" /> <label for="R6" class="name1" style="color:white;">Le Blanc</label><br />
-          <input type="radio" name="Q1" value="R7" id="R7" /> <label for="R7" class="name1" style="color:orange;">Le Orange</label><br />
-          <input type="radio" name="Q1" value="R8" id="R8" /> <label for="R8" class="name1" style="color:yellow;">Le Jaune</label><br />
-          <input type="radio" name="Q1" value="R9" id="R9" /> <label for="R9" class="name1" style="color:brown;">Le Marron</label><br />
-          <input type="radio" name="Q1" value="R10" id="R10" /> <label for="R10" class="name1" style="color:silver;">Le Gris</label><br />
+          <input type="radio" name="Q1" value="R1" id="Q1R1" /> <label for="Q1R1" class="name1">Le Noir</label><br />
+          <input type="radio" name="Q1" value="R2" id="Q1R2" /> <label for="Q1R2" class="name1" style="color:aqua;">Le Bleu</label><br />
+          <input type="radio" name="Q1" value="R3" id="Q1R3" /> <label for="Q1R3" class="name1" style="color:red;">Le Rouge</label><br />
+          <input type="radio" name="Q1" value="R4" id="Q1R4" /> <label for="Q1R4" class="name1" style="color:purple;">Le Violet</label><br />
+          <input type="radio" name="Q1" value="R5" id="Q1R5" /> <label for="Q1R5" class="name1" style="color:lime;">Le Vert</label><br />
+          <input type="radio" name="Q1" value="R6" id="Q1R6" /> <label for="Q1R6" class="name1" style="color:white;">Le Blanc</label><br />
+          <input type="radio" name="Q1" value="R7" id="Q1R7" /> <label for="Q1R7" class="name1" style="color:orange;">Le Orange</label><br />
+          <input type="radio" name="Q1" value="R8" id="Q1R8" /> <label for="Q1R8" class="name1" style="color:yellow;">Le Jaune</label><br />
+          <input type="radio" name="Q1" value="R9" id="Q1R9" /> <label for="Q1R9" class="name1" style="color:brown;">Le Marron</label><br />
+          <input type="radio" name="Q1" value="R10" id="Q1R10" /> <label for="Q1R10" class="name1" style="color:silver;">Le Gris</label><br />
           
+          <p>Quel objet vous correspond le mieux ?</p>
+          <input type="radio" name="Q1" value="R1" id="Q2R1" /> <label for="R1" class="name1"><img src="pics/items/Ender_Pearl.phn" alt="" width="30px" />La Perle Noire.</label><br />
         </form>
       <?php
       }
