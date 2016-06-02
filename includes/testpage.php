58827c6a33d33aa31f339a1562c9928ab986dfcd
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-02 11:15:18 +0200

    Update testpage.php

--- includes/testpage.php
+++ includes/testpage.php
@@ -1,5 +1,28 @@
 <?php function testpage()
 {
-
+  if ($_SESSION['connected'])
+  {
+    $verif = $db->prepare('SELECT id, testm FROM members WHERE id = ? AND testm = 0');
+    $verif->execute(array($_SESSION['id']));
+    if ($verif->fetch())
+    {
+      if (isset($_POST['valid']))
+      {
+        
+      }
+      else
+      {
+        
+      }
+    }
+    else
+    {
+      echo '<p>NAvré, mais vous avez déjà passé ce test.</p>';
+    }
+  }
+  else
+  {
+    echo '<p>Vous devez vous connecter pour accéder à cette page.</p>';
+  }
 }
 ?>
