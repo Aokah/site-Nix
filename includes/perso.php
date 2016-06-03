9866a0274e0d645e856a8b885c8004b326eb24db
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-03 17:48:00 +0200

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -871,7 +871,11 @@
 								
 							<tr>
 								<td colspan="2">
-									<p>Magie : <? echo $magieok; ?></p>
+									<p>Magie : <? echo $magieok; if ($line['magieok'] == 0) {?>
+									<a title="Activer la Maîtrise de la Magie" href="index?p=perso&perso=<?php echo $perso; ?>&action=magieok" style="color:blue;">[mOK]</a>
+									<?php } else { ?>
+									<a title="Désactiver la Maîtrise de la Magie" href="index?p=perso&perso=<?php echo $perso; ?>&action=magieko" style="color:red;">[mKO]</a> 
+									<?php } } ?></p>
 								</td>
 							</tr>
 							<tr>
