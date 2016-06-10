48760b184a0e0f3f21218bf75dcd4947476f1624
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-10 13:41:29 +0200

    Update report.php

--- includes/report.php
+++ includes/report.php
@@ -1,5 +1,24 @@
 <?php function report ()
 {
-
+  if ($_SESSION['connected'])
+  {
+    echo '<h2>Rapport d\'erreur</h2>';
+   # if ($_SESSION['rank'] > 4)
+    #{
+      // Affichage MJ
+    #}
+    #else
+    #{
+    ?>
+      <p>Ici vous pourrez exposer vos problèmes / bugs trouvés sur le site ou le serveur afin que le Staff puisse le régler quand il le verra.</p>
+      <p>Tout abus ou usage malhonnête de cette page pourra amener à des sanctions.</p>
+      
+    <?php
+    #}
+  }
+  else
+  {
+    echo '<p>Vous devez être connecté pour avoir accès à cette page.</p>';
+  }
 }
 ?>
