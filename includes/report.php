b518bf834a8dc852ac4328c9b64f48bcb025bc92
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-11 02:47:05 +0200

    Update report.php

--- includes/report.php
+++ includes/report.php
@@ -1,5 +1,7 @@
 <?php function report ()
 {
+  global $_POST, $db;
+  
   if ($_SESSION['connected'])
   {
     echo '<h2>Rapport d\'erreur</h2>';
@@ -12,7 +14,20 @@
     ?>
       <p>Ici vous pourrez exposer vos problèmes / bugs trouvés sur le site ou le serveur afin que le Staff puisse le régler quand il le verra.</p>
       <p>Tout abus ou usage malhonnête de cette page pourra amener à des sanctions.</p>
-      
+      <form action="index?p=report" method="POST">
+        <label for="type">Sélectionnez votre type de problème : </label><select name="type">
+          <option value="1">Bug In Game</option>
+          <option value="2">Grief In Game</option>
+          <option value="3">RPQ sur le serveur</option>
+          <option value="4">Tricherie explicite</option>
+          <option value="5">Abus de pouvoir d'un Staffeux</option>
+          <option value="6">"Fatal Error" sur le site</option>
+          <option value="7">Bug d'affichage site</option>
+          <option value="8">Autres . . .</option>
+        </select>
+        <textarea placeholder="Notez ici le problème que vous avez rencontré . . ." name="report" style="width: 500px; height: 200px;" align="center"></textarea>
+        <br /><input type="submit" name="send" value="Envoyer" />
+      </form>
     <?php
     #}
   }
