4bd4cdfd7a28cd7ff97ba74292d1ebd4ad69e989
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-11 03:29:13 +0200

    Update report.php

--- includes/report.php
+++ includes/report.php
@@ -5,21 +5,25 @@
   if ($_SESSION['connected'])
   {
     echo '<h2>Rapport d\'erreur</h2>';
-   # if ($_SESSION['rank'] > 4)
-    #{
-      // Affichage MJ
-    #}
-    #else
-    #{
+   
     if (isset($_POST['send']))
     {
       $report = htmlspecialchars($_POST['report']);
-      $insert = $db->prepare("INSERT INTO report VALUES('',?,?,?,1,0)"); $insert->execute(array($_POST['type'], $report, $_SESSION['id']));
+      $insert = $db->prepare("INSERT INTO report VALUES('',NOW(),?,?,?,1,0,'','')"); $insert->execute(array($_POST['type'], $report, $_SESSION['id']));
       echo '<p>Votre rapport a bien été enregistré.<p>
       <p><a href="index?p=report">Cliquez ici</a> pour retourner à la page précédente.</p>';
     }
     else
     {
+      if ($_SESSION['rank'] > 4)
+      {
+       ?>
+       <div>
+         <h3>Liste des</h3>
+       </div>
+       <?
+      }
+    
     ?>
       <p>Ici vous pourrez exposer vos problèmes / bugs trouvés sur le site ou le serveur afin que le Staff puisse le régler quand il le verra.</p>
       <p>Tout abus ou usage malhonnête de cette page pourra amener à des sanctions.</p>
@@ -37,9 +41,47 @@
         <div align="center"><textarea placeholder="Notez ici le problème que vous avez rencontré . . ." name="report" style="width: 500px; height: 200px;"></textarea></div>
         <br /><input type="submit" name="send" value="Envoyer" />
       </form>
-    <?php
+      <?php
+      $presel = $db->prepare('SELECT COUNT(*) AS count FROM report WHERE reporter_id = ?'); $presel->eecute(array($_SESSION['id']));
+      $presel = $presel->fetch();
+      if ($presel['count'] > 0)
+      {
+        ?>
+        <h3>Requête de votre part classées par date d'envoi</h3>
+        <?php
+        $select = $db->prepare('SELECT * FROM report WHERE sender_id = ?'); $select->execute(array($_SESSION['id']));
+        while ($line = $select->fetch())
+        {
+        ?>
+        <table cellspacing="0" cellpadding="0" width="100%" style="border: 5px gray solid; border-radius: 10px; background-color: #DDDDDD;text-shadow: white 1px 1px 4px;">
+          <tbody>
+            <tr style="background-color:#BBBBBB;">
+              <th><?= $type?></th>
+            </tr>
+            <tr>
+              <td>Etat d'analyse du problème : <img src="pics/ico/<?= $state?>" alt="" title="<?= $desc?>"/> </td>
+            </tr>
+            <tr>
+              <td>Date d'envoi : <?= $date?></td>
+            </tr>
+            <tr>
+              <td>Problème :
+              <p><?= $report?></p></td>
+            </tr>
+            <tr>
+              <td>Date de réponse : <?= $date_?></td>
+            </tr>
+            <tr>
+              <td>Réponse :
+              <p><?= $respond?></p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       <?php
+        }
+      }
     }
-    #}
   }
   else
   {
