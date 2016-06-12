721bea8b3092c89052fecbad8f8dc046ce9549a0
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-12 23:47:55 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,12 +1,6 @@
 <?php function testpage_2 ()
 {
 global $db;
-$select = $db->query('SELECT * FROM members');
-while ($line = $select->fetch())
-{
-$insert = $db->prepare("INSERT INTO caract VALUES('',?, 100,0,0,0,0,0,0)");
-$insert->execute(array($line['id']));
-echo 'Succès';
-}
+
 }
 ?>
