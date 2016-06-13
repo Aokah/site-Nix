a7b8b755644bb8dede89c4d829043705a6a67366
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-13 19:35:27 +0200

    Update credits.php

--- includes/credits.php
+++ includes/credits.php
@@ -2,14 +2,14 @@
 {
 ?>
 	<h2>Remerciements divers</h2>
-	<p>Ici seront répertoriées les différents noms des personnes ayant contribué plus ou moins à Nix.</p>
+	<p>Ici seront répertoriés les différents noms des personnes ayant contribué plus ou moins à Nix.</p>
 	
 	<table cellspacing="0" cellpadding="5" width="80%" align="center" style="border: 5px gray solid; border-radius: 10px; background-color: #DDDDDD;text-shadow: white 1px 1px 4px;">
 		<tbody>
 			<tr style="background-color:#BBBBBB;">
 				<th colspan="2">Remerciements de Construction</th>
 			</tr>
-			<tr>
+			<!--<tr>
 				<th colspan="2">Event : Les Souvenirs d'Eralion</th>
 			</tr>
 			<td><ul>
@@ -19,6 +19,8 @@
 				<li class="name1" style="color:white">Herborus</li>
 				<li class="name1" style="color:white">Kindra</li>
 				<li class="name1" style="color:white">Kuda</li>
+				<li class="name1" style="color:white">Niyo</li>
+				<li class="name1" style="color:white">Shakha</li>
 				<li class="name1" style="color:white">Shawn</li>
 				<li class="name1" style="color:white">Shimmel</li>
 				<li class="name1" style="color:white">Shonrei</li>
@@ -26,7 +28,7 @@
 			</ul></td>
 			<td>
 				<img src="#" alt="" width="100%"/>
-			</td>
+			</td>-->
 		</tbody>
 	</table>
 <?php
