6d2844baa468f2803020ebf144e4a8c0b3be0527
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-17 22:37:10 +0200

    Update event.php

--- includes/event.php
+++ includes/event.php
@@ -23,6 +23,7 @@ global $db, $_SESSION, $_POST, $_GET;
         			<th>Intitulé</th>
         			<th>Nature</th>
         			<th>Lancement</th>
+        			<th>Lanceur</th>
         		</tr>
         		<?php
         		while ($line = $select->fetch())
@@ -38,14 +39,20 @@ global $db, $_SESSION, $_POST, $_GET;
 		        		case 6 : $type = "Event Donjon"; break;
 		        		case 7 : $type = "Event Expédition"; break;
 		        	}
+		        	$select_ = $db->prepare('SELECT * FROM members WHERE id = ?'); $select_->execute(array($line['user_id']));
+		        	$line_ = $select_->fetch();
+		        	if ($line_['pionier'] == 1) { $title ="Pionier"; } elseif ($line_['ban'] == 1) { $title = "Banni"; } 
+		        	elseif ($line_['remove'] == 1) { $title = "Oublié"; } else { $title = $line_['title']; }
+		        	if ($line_['ppionier'] == 1) { $pionier = "-P"; } if ($line_['technician'] == 1) { $tech = "-T"; }
 		        ?>
 		        <tr style="text-align:center;">
 		        	<td><a href="index?p=event&e=<?=$line['id']?>"><?=$line['name']?></a></td>
 		        	<td><?=$type?></td>
 		        	<td><?=$line['begin']?></td>
+		        	<td class="name<?= $line_['rank'], $pionier, $tech?>"><?=$title, ' ', $line_['name']?></td>
 		        </tr>
 		        <tr>
-		        	<td colspan="3">
+		        	<td colspan="4">
 		        		<div style="border-bottom:3px dashed black;margin-bottom:1%;margin-top:1%;"></div>
 		        	</td>
 		        </tr>
