3ca42cfa3accb6124fafb4c62d681c61dd3200f9
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-17 20:41:19 +0200

    Update event.php

--- includes/event.php
+++ includes/event.php
@@ -1,6 +1,48 @@
 <?php function event ()
 {
 global $db, $_SESSION, $_POST, $_GET;
-
+   if ($_SESSION['connected'])
+   {
+     if ($_SESSION['rank'] > 4)
+     {
+      ?>
+      <h2>Evènements Nix</h2>
+      <p>Ici sont écrits en plus amples détails tout ce qui a à savoir sur tel ou tel évènement lié au serveur !</p>
+      <?php
+        $select = $db->query('SELECT * FROM event ORDER BY name ASC');
+        ?>
+        <table cellspacing="0">
+        	<tbody>
+        		<tr>
+        			<th>Intitulé</th>
+        			<th>Nature</th>
+        			<th>Lancement</th>
+        		</tr>
+        		<?php
+        		while ($line = $select->fetch())
+		        {
+		        ?>
+		        <tr>
+		        	<td><a href="index?p=event&e=<?=$line['id']?>"><?=$line['name']?></a></td>
+		        	<td><?=$type?></td>
+		        	<td><?=$begin?></td>
+		        </tr>
+		        <?php
+    			 }
+    			 ?>
+        	</tbody>
+        </table>
+        <?php
+        
+     }
+     else
+     {
+       echo '<p<Navré, mais vous n\'avez pas le niveau suffisant pour visionner cette page.</p>';
+     }
+   }
+   else
+   {
+     echo '<p>Veuillez vous connecter pour accéder à cette page.</p>';
+   }
 }
 ?>
