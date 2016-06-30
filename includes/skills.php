e51357daf4f94fb5179c0b7d81cc97623d80e644
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-30 16:08:05 +0200

    Update skills.php

--- includes/skills.php
+++ includes/skills.php
@@ -156,10 +156,18 @@
       ?>
       <h2>Mes Aptitudes</h2>
       <p>Cette page vous servira à répartir vos différents points de compétence et de vous informer sur les aptitudes de votre personnage dans le domaine de la Magie.</p>
+      <? if ($select['specialisation'] == "Inconnue")
+      {
+       ?> <p class="name3">Vous n'avez actuellement aucune spécialisation magique, il est préférable d'attendre d'avoir connu votre affinité avant d'attribuer des Points de Compétences (PCs)</p>
+      }
+      else
+      {
+      ?>
       <p class="name3">Vous êtes actuellement spécialisé en <span class="name6"><?=$select['specialisation']?></span><?
       if ($select['spe_2'] != "Inconnue") {?> et <span class="name6"><?=$select['spe_2']?></span><?php } ?>. Il est donc vivement conseillé d'attribuer vos points dans <?php
       if ($select['spe_2'] != "Inconnue") { echo 'ces '; } else { echo 'ce '; } ?> domaine<?php
       if ($select['spe_2'] != "Inconnue") { echo 's'; }?>.</p>
+      <? } ?>
       <table cellspacing="0" cellpadding="10" width="100%">
       	<tbody>
       	  <tr>
