34b394b94e01bbb5b31a3affb9070630aad77b2f
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-06-30 21:34:52 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -7,7 +7,7 @@
 	
 	$norma = $db->prepare('SELECT id, puis_norma FROM members WHERE id = ?'); $norma->execute(array($for));
 	$norma = $norma->fetch();
-	$incan = $db->prepare('SELECT id, norma WHERE incan_list WHERE id = ?'); $incan->execute(array($id));
+	$incan = $db->prepare('SELECT id, norma FROM incan_list WHERE id = ?'); $incan->execute(array($id));
 	$incan = $incan->fetch();
 	$result = $norma['puis_norma'] + $incan['norma']; 
 	
