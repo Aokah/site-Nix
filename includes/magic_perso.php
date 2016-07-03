8a0ec3dc644e421cecc90468802e4fbd742c8c06
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-03 16:32:07 +0200

    Update magic_perso.php

--- includes/magic_perso.php
+++ includes/magic_perso.php
@@ -1,8 +1,16 @@
 <?php function magic_level ()
 {
 	global $db;
+	if (isset($_GET['perso']))
+	{
+		$id = intval($_GET['perso']);
+	}
+	else
+	{
+		$id = $_SESSION['id'];
+	}
 	
-	$select = $db->prepare('SELECT * FROM magic_level WHERE id = ? AND spe = 1'); $select->execute(array($_SESSION['id']));
+	$select = $db->prepare('SELECT * FROM magic_level WHERE id = ? AND spe = 1'); $select->execute(array($id));
 	while ($line = $select->fetch())
 	{
 ?>
@@ -23,7 +31,7 @@
 	}
 	if ($line['spe_2'] != "Inconnue")
 	{
-		$select = $db->prepare('SELECT * FROM magic_level WHERE id = ? AND spe = 2'); $select->execute(array($_SESSION['id']));
+		$select = $db->prepare('SELECT * FROM magic_level WHERE id = ? AND spe = 2'); $select->execute(array($id));
 		while ($line = $select->fetch())
 		{
 		?>
