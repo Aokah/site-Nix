60a8e7c9b4edee0d575cc0fa0c89b3abbd018411
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 16:19:56 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -3,7 +3,16 @@
 	global $db; 
 	
 	echo "<h2>CB</h2>";
-	
+	?>
+		<div align="center" class="memberbg_4">
+			<form action="cb_form" method="POST">
+				<label for="cb_msg">Message : </label> <input type="text" name="cb_msg" id="cb_msg" /></br>
+				<label for="cb_whisp">Message : </label> <input type="text" name="cb_whisp" id="cb_whisp" />
+				<label for="cb_salon">Message : </label> <input type="text" name="cb_salon" id="cb_salon" />
+				<input type="submit" value="Envoyer" />
+			</form>
+		</div>
+	<?php
 	
 }
 ?>
