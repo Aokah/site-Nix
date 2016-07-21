c992c49570e12bf1afcbc0c7582d4e41eb06094c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 19:01:27 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -75,7 +75,7 @@
 				<label for="cb_msg">Message : </label> <input type="text" name="cb_msg" id="cb_msg" /></br>
 				<label for="cb_whisp">Chuchoter à : </label> <input type="text" name="cb_whisp" id="cb_whisp" />
 				<label for="cb_salon">Salon : </label> <input type="text" name="cb_salon" id="cb_salon" />
-				<input type="submit" value="Envoyer" />
+				<input type="submit" name="send_cb" value="Envoyer" />
 			</form>
 		</div>
 	<?php
