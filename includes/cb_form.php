6e2a157aa5dfef62043b4329db5df94a5c226d49
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 18:47:52 +0200

    Update cb_form.php

--- includes/cb_form.php
+++ includes/cb_form.php
@@ -2,6 +2,10 @@
 {
 	global $db, $_POST, $_SESSION;
 	
-	$req = $db->prepare("INSTER INTO cb_test VALUES('',)");
+	$req = $db->prepare("INSTER INTO cb_test VALUES('', ?, ?, '', 0, ?, ?, NOX(), 0)");
+	$req->execute(array($_SESSION['id'], $_POST['cb_whisp'], htmlspecialchars($_POST['cb_msg']), htmlspecialchars($_POST['effect']),
+	htmlspecialchars($_POST['option'] )));
+	
+	header('Location: testpage_2.php');
 }
 ?>
