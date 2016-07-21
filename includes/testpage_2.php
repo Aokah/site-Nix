5c3776bf09d691cdb199a79048ecb759dd471192
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 17:08:51 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -20,7 +20,7 @@
 				 	$pionier = ($name_cb['pionier'] == 1)? '-P' : '';
 				?>
 				<span class="name<?= $name_cb['rank'], $tech, $pionier; ?>"><?= $name_cb['name']?></span> : <span <?= $bonus
-				?>><? $line['message']?></span>
+				?>><?= $line['message']?></span>
 				<?php
 				 }
 				 ?>
