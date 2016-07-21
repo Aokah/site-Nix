ce239e6c41acafdcbd5061e697a6a6f7d1d5378c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 19:11:32 +0200

    Update cb_form.php

--- includes/cb_form.php
+++ includes/cb_form.php
@@ -3,10 +3,17 @@
 	global $db, $_POST, $_SESSION;
 	if (isset($_POST['send_cb']) AND isset($_POST['cb_msg']))
 	{
+		$to = (isset($_POST['cb_whisp'])) ? $_POST['cb_whisp'] : 0;
+		if (isset($_POST['cb_whisp']))
+		{
+			$presel = $db->prepare('SELECT id, name FROM members WHERE name = ?'); $presel->execute(array($to))
+			$line = $presel->fetch();
+			$to = $line['id'];
+		}
 		$effect = (isset($_POST['effect'])) ? $_POST['effect'] : '';
 		$option = (isset($_POST['option'])) ? $_POST['option'] : '';
 		$req = $db->prepare("INSTER INTO cb_test VALUES('', ?, ?, '', 0, ?, ?, NOX(), 0)");
-		$req->execute(array($_SESSION['id'], $_POST['cb_whisp'], htmlspecialchars($_POST['cb_msg']), htmlspecialchars($effect),
+		$req->execute(array($_SESSION['id'], $to , htmlspecialchars($_POST['cb_msg']), htmlspecialchars($effect),
 		htmlspecialchars($option)));
 	}
 	
