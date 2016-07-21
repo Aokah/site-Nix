7cd64a35bd1c75f96c839284e669df8c14048fc6
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 17:19:40 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -20,7 +20,7 @@
 				 	$pionier = ($name_cb['pionier'] == 1)? '-P' : '';
 				 	$date_send = preg_replace('#^.{11}(.{2}):(.{2}):.{2}$#', '$1:$2', $line['post_date']);
 				?>
-				<p syle="text-align:left;">
+				<p style="text-align:left;">
 				<?= $date_send; ?> : <span class="name<?= $name_cb['rank'], $tech, $pionier; ?>"><?= $name_cb['name']?></span> : <span <?= $bonus
 				?>><?= $line['message']?></span>
 				</p>
