3840bd891d7d76a6500be57bd46f03b458320f76
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 19:23:06 +0200

    Update cb_form.php

--- includes/cb_form.php
+++ includes/cb_form.php
@@ -3,15 +3,19 @@
 	global $db, $_POST, $_SESSION;
 	if (isset($_POST['send_cb']) AND isset($_POST['cb_msg']))
 	{
-		$to = (isset($_POST['cb_whisp'])) ? $_POST['cb_whisp'] : '0';
 		if (isset($_POST['cb_whisp']))
 		{
 			$presel = $db->prepare('SELECT id, name FROM members WHERE name = ?'); $presel->execute(array($to));
 			$line = $presel->fetch();
 			$to = $line['id'];
+			$to = $_POST['cb_whisp'];
 		}
-		$effect = (isset($_POST['effect'])) ? $_POST['effect'] : " ";
-		$option = (isset($_POST['option'])) ? $_POST['option'] : " ";
+		else
+		{
+			$to = "0";
+		}
+		$effect = (isset($_POST['effect']))? $_POST['effect'] : " ";
+		$option = (isset($_POST['option']))? $_POST['option'] : " ";
 		$req = $db->prepare("INSERT INTO cb_test VALUES('', ?, ?, '', 0, ?, ?,  ?,  NOW(), 0)");
 		$req->execute(array($_SESSION['id'], $to , htmlspecialchars($_POST['cb_msg']), htmlspecialchars($effect),
 		htmlspecialchars($option)));
