268c773996988b500e7bd6a5bf4523045cc81e60
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-21 16:11:25 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -2,19 +2,7 @@
 {
 	global $db; 
 	
-	$id = 50;
-	$for =  204;
-	
-	$norma = $db->prepare('SELECT id, puis_norma FROM members WHERE id = ?'); $norma->execute(array($for));
-	$norma = $norma->fetch();
-	$incan = $db->prepare('SELECT id, norma FROM incan_list WHERE id = ?'); $incan->execute(array($id));
-	$incan = $incan->fetch();
-	$result = $norma['puis_norma'] + $incan['norma']; 
-	
-	echo $norma['puis_norma'], ' + ', $incan['norma'], ' = ', $result, "<br /><br />";
-	
-	$pcs = ($result / 3) * 100000000000000;
-	echo number_format($pcs, 0);
+	echo "<h2>CB</h2>";
 	
 	
 }
