81ee935e2c20220994d0fee43663015744b0b16d
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-26 21:23:40 +0200

    Update perso.php

--- includes/perso.php
+++ includes/perso.php
@@ -1215,8 +1215,8 @@ if ($_SESSION['connected'])
 					$countj = $line0['plus'] - $line2['moins'];
 					$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
 					$counts = $plus - $moins; $hrpavis = $countj + $counts;
-					if ($hrpavis >= 10) { $coloravis = 10;} elseif ($hrpavis >= 20) { $coloravis = 20;} elseif ($hrpavis >= 30) { $coloravis = 30;} elseif ($hrpavis < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
-				?>
+					if ($hrpavis >= 10 AND $hrpavis < 20) { $coloravis = 10;} elseif ($hrpavis >= 20 AND $hrpavis < 30) { $coloravis = 20;} elseif ($hrpavis >= 30) { $coloravis = 30;} elseif ($hrpavis < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
+					?>
 				<td valign="top" width="50%" rowspan="<?php echo $span; ?>">
 					<table width="640px" cellspacing="5" cellpading="5" class="pnjtable" align="center">
 						<tbody>
@@ -2169,8 +2169,8 @@ if ($_SESSION['connected'])
 					$countj = $line0['plus'] - $line2['moins'];
 					$plus = $line1['plusstaff'] * 2; $moins = $line3['moinsstaff'] * 2;
 					$counts = $plus - $moins; $hrpavis = $countj + $counts;
-					if ($hrpavis >= 10) { $coloravis = 10;} elseif ($hrpavis >= 20) { $coloravis = 20;} elseif ($hrpavis >= 30) { $coloravis = 30;} elseif ($hrpavis < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
-				?>
+					if ($hrpavis >= 10 AND $hrpavis < 20) { $coloravis = 10;} elseif ($hrpavis >= 20 AND $hrpavis < 30) { $coloravis = 20;} elseif ($hrpavis >= 30) { $coloravis = 30;} elseif ($hrpavis < 0) { $coloravis = "negative"; } else { $coloravis = 0; }
+					?>
 				<td valign="top" width="50%" rowspan="<?php echo $span; ?>">
 					<table align="center" width="640px" cellspacing="5" cellpading="5" class="pnjtable">
 						<tbody>
