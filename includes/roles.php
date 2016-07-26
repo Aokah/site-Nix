55b57796b9f29714182a17d98b70abccb24b63bc
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-26 19:04:59 +0200

    Update roles.php

--- includes/roles.php
+++ includes/roles.php
@@ -6,33 +6,34 @@
 	{
 		if ($_SESSION['rank'] > 4)
 		{
-			$select = $db->query('SELECT * FROM members WHERE rank > 4 AND dignitaire = 0 ORDER BY Rank DESC');
+			$select = $db->query('SELECT * FROM members WHERE rank > 4 AND AND rank < 8 dignitaire = 0 ORDER BY Rank DESC');
 			?>
-			<table calss="member_top">
+			<table class="member_top">
 				<tbody>
 					<tr>
 						<th>Staffeux</th> <th>Tâche assignée</th> <th>Détails</th> <th>Statut de la tâche</th>
 					</tr>
-					<tr>
-						<?php
-						while ($line = $select->fetch())
+					
+					<?php
+					while ($line = $select->fetch())
+					{
+						$presel = $db->prepare('SELECT * FROM roles WHERE user_id = ?');
+						$presel->execute(array($line['id']));
+						if ($line_ = $presel->fetch())
 						{
-							$presel = $db->prepare('SELECT * FROM roles WHERE user_id = ?');
-							$presel->execute(array($line['id']));
-							if ($line_ = $presel->fetch())
-							{
-								$role = $line_['name'];
-								$desc = $line_["descritpion"];
-								$state = $line_["state"];
-							}
-							else
-							{
-								$role = "Pas encore de tâche donnée";
-								$desc = "Aucune description disponible.";
-								$state = "N/A";
-							}
-						?>
-						<td class="memberbg_<?= $line['rank']?>">
+							$role = $line_['name'];
+							$desc = $line_["descritpion"];
+							$state = $line_["state"];
+						}
+						else
+						{
+							$role = "Pas encore de tâche donnée";
+							$desc = "Aucune description disponible.";
+							$state = "N/A";
+						}
+					?>
+					<tr class="memberbg_<?= $line['rank']?>">
+						<td>
 							<span class="name<?= $line['rank']?>"><?= $line['name']?></span>
 						</td>
 						<td>
@@ -50,10 +51,10 @@
 								<?= $state?>
 							</P>
 						</td>
-						<?php
-						}
-						?>
 					</tr>
+					<?php
+					}
+					?>
 				</tbody>
 			</table>
 			<?php
