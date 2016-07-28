9746a6d8f0f04d627c9acd001d89bc9320be0458
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-29 00:08:06 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -1,41 +1,7 @@
 <?php function testpage_3 ()
 {
-	global $db;
 	
-	include('includes/interface/JSONapi.php');
-		$ip = 'soul.omgcraft.fr';
-		$port = 20059;
-		$user = "nix";
-		$pwd = "dragonball";
-		$salt = 'salt';
-		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
-		
-		if (isset($_GET['action']))
-		{
-			if ($_GET['test'])
-			{
-				$select = $db->prepare('SELECT id, Minecraft_Account AS mc FROM members WHERE id = ?');
-				$select->execute(array($_SESSION['id'])); $line = $select->fetch();
-				$test = "Laura_Dragon";
-				$api->call("run.server_command", array("say test"));
-				echo '<img src="http://skins.minecraft.net/MinecraftSkins/'.$line['mc'].'.png" alt = "" />';
-			}
-		}
-		else
-		{
-			$PlayerNames = $api->call("getPlayerNames");
-			if ($PlayerNames)
-			{
-				$mc = ($PlayerNames['success']);
-				
-			}
-			else
-			{
-				echo '<p>Aucun joueur n\'est connecté.</p>';
-			}
-			echo '<a href="index?p=testpage_3&action=test">Test /nick</a>';	
-		}
-		
-
+	echo "<h3>Forums</h3>";
+	
 }
 ?>
