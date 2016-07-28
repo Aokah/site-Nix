f3d7ef52060893f32803ce8bd71d722a4633ea89
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-29 00:45:53 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -6,6 +6,35 @@
 	
 	$view = (isset($_SESSION['rank'])) ? $_SESSION['rank'] : 0;
 	
-	echo $view;
+	if (isset($_GET['cat']))
+	{
+		
+	}
+	elseif (isset($_GET['forum']))
+	{
+		
+	}
+	else
+	{
+		$select = $db->prepare('SELECT * FROM forum_category WHERE rank = ? AND group = 0'); $select->execute(array($view));
+		?>
+		<div width="100%" style"padding:1%;margin:1%;">
+			<?php
+			while ($line = $select->fetch())
+			{
+			?>
+			<h4><?= $line['name']?></h4>
+			<p><img src="pics/forumcat_<?= $line['id']?>.png" class="guild" /></p>
+			<table cellspacing="0" cellpadding="1%" align="center" width="95%">
+				<tbody>
+					<tr></tr>
+				</tbody>
+			</table>
+			<?php
+			}
+			?>
+		</div>
+		<?php
+	}
 }
 ?>
