5aac1b4038ebe8eb16428d64bd1f5b6121c43ef6
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-31 20:58:33 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -1,5 +1,14 @@
 <?php function testpage_2 ()
 {
-
+	
+	include('includes/interface/JSONapi.php');
+		$ip = 'soul.omgcraft.fr';
+		$port = 20059;
+		$user = "nix";
+		$pwd = "dragonball";
+		$salt = 'salt';
+		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
+	
+	$api->call("sendCommand", array("weather clear"));
 }
 ?>
