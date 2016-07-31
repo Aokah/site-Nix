bf17bf05aa9a190f2d4d04403721cfedde1211a8
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-31 03:00:26 +0200

    Update cb_form.php

--- includes/cb_form.php
+++ includes/cb_form.php
@@ -16,8 +16,8 @@
 		}
 		if (isset($_POST['staffeffect']))
 		{
-		$effect = (isset($_POST['effect']))? $_POST['effect'] : " ";
-		$option = (isset($_POST['option']))? $_POST['option'] : " ";
+		$effect = (isset($_POST['effect']) AND $_SESSION['rank'] >= 5)? $_POST['effect'] : " ";
+		$option = (isset($_POST['option']) AND $_SESSION['rank'] >= 5)? $_POST['option'] : " ";
 		}
 		$req = $db->prepare("INSERT INTO cb_test VALUES('', ?, ?, '', 0, ?, ?,  ?,  NOW(), 0)");
 		$req->execute(array($_SESSION['id'], $to , htmlspecialchars($_POST['cb_msg']), htmlspecialchars($effect),
@@ -26,8 +26,17 @@
 	if (isset($_GET['del']))
 	{
 		$id = intval($_GET['del']);
-		$del =$db->prepare('UPDATE cb_test SET del = 1, deleter_id = ? WHERE id = ?');
-		$del->execute(array($_SESSION['id'], $id));
+		
+		$presel = $db->prepare('SELECT id, sender_id FROM cb_test WHERE id = ?'); $presel->execute(array($id));
+		$presel = $presel->fetch();
+		$verif = $db->prepare('SELECT id, rank FROM members WHERE id = ?'); $verif->execute(array($presel['sender_id']));
+		$verif = $verif->fetch();
+		
+		if ($_SESSION['rank'] >= $verif['rank'])
+		{
+			$del =$db->prepare('UPDATE cb_test SET del = 1, deleter_id = ? WHERE id = ?');
+			$del->execute(array($_SESSION['id'], $id));
+		}
 	}
 	
 }
