15d5e6ae72642e853b8108c3856d5e6019eae540
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-31 21:03:17 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -9,6 +9,6 @@
 		$salt = 'salt';
 		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
 	
-	$api->call("sendCommand", array("weather clear"));
+	$api->call("sendCommand", array("/weather clear"));
 }
 ?>
