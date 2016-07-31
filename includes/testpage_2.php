6f717c42004a2b53153919fd841c5671f731164a
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-31 21:57:55 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -9,6 +9,6 @@
 		$salt = 'salt';
 		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
 	
-	$api->call("runConsoleCommand", array("gamemode", "chugo 3"));
+	$api->call("runConsoleCommand", array("gamemode", "3 chugo"));
 }
 ?>
