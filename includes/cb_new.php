3a11f8a358118f2898dc573b0c8353af51597099
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-07-31 02:54:49 +0200

    Update cb_new.php

--- includes/cb_new.php
+++ includes/cb_new.php
@@ -2,21 +2,24 @@
 {
 	global $db; 
 	include ('includes/cb_form.php'); cb_form();
-	echo "<h2>CB</h2>";
+	echo "<h2>CB (alpha)</h2>";
 	?>
 		<div align="center" width="95%" class="memberbg_6">
 			<form method="POST" action="index?p=cb" style="text-align:left; padding:1%">
 				<label for="cb_msg">Message : </label> <input type="text" id="reloadCB" onclick="toggleAutoRefresh(this);" name="cb_msg" id="cb_msg" /></br>
-				<label for="cb_whisp">Chuchoter à : </label> <input type="text" name="cb_whisp" id="cb_whisp" /><br />
+			<!--	<label for="cb_whisp">Chuchoter à : </label> <input type="text" name="cb_whisp" id="cb_whisp" /><br />-->
 			<!--	<label for="cb_salon">Salon : </label> <input type="text" name="cb_salon" id="cb_salon" /> -->
-				<? if ($_GET['admin'] != "see_del")
-					{
-						echo '<a href="index?p=cb&admin=see_del" class="button">Voir les messages supprimés</a>';
-					}
-				if (isset($_GET['admin']))
-					{
-						echo '<a href="index?p=cb" class="button">Retour à la CB Classique</a>';
-					}
+				<?
+				if ($_SESSION['rank'] > 5)
+				{
+					if ($_GET['admin'] != "see_del")
+						{
+							echo '<a href="index?p=cb&admin=see_del" class="button">Voir les messages supprimés</a>';
+						}
+					if (isset($_GET['admin']))
+						{
+							echo '<a href="index?p=cb" class="button">Retour à la CB Classique</a>';
+						}
 				?>
 				<br /><input type="checkbox" name="staffeffect" id="staffeffect" /> <label for="staffeffect">Activer un effet de texte (NECESSITE DES BASES EN HTML)</label><br />
 				<select name="effect">
@@ -24,47 +27,30 @@
 					<option value="style">Style</option>
 				</select>
 				<input type="text" name="option"/>
+				<?php } ?>
 				<br /><input type="submit" name="send_cb" value="Envoyer" /> <input type="submit" name="refresh" value="Reload" />
 			</form>
 			
 			<div width="100%" style="padding:1%;" class="memberbg_4">
 				<?php
-				if (isset($_GET['admin']))
+				if (isset($_GET['admin']) AND $_SESSION['rank'] > 5)
 				{
 					if ($_GET['admin'] == "see_del")
 					{
-					#	$count = $db->prepare('SELECT COUNT(*) AS count FROM cb_test WHERE salon = "" AND to_id = ? OR
-					 #	 salon = "" AND to_id = 0 ORDER BY post_date ASC');
-					 #	 $count->execute(array($_SESSION['id']));
-					 #	$limit1 = $count['count'];
-					 #	$limit2 = $limit1 - 20;
-					 #	$limit2 = ($limit2 < 0) ? 0 : $limit2;
 					 	
 						$cb_select = $db->prepare('SELECT * FROM cb_test WHERE salon = "" AND to_id = ? OR
-					 	 salon = "" AND to_id = 0 ORDER BY post_date DESC LIMIT 0, 20');
+					 	 salon = "" AND to_id = 0 ORDER BY post_date DESC LIMIT 20');
 					 	$cb_select->execute(array($_SESSION['id']));	
 					}
 					elseif ($_GET['admin'] == "see_whisp")
 					{
-					#	$count = $db->prepare('SELECT COUNT(*) AS count FROM cb_test WHERE del = 0 AND salon = ""  ORDER BY post_date ASC ');
-					 #	$count->execute(array($_SESSION['id'])); $count = $count->fetch();
-					 #	$limit1 = $count['count'];
-					 #	$limit2 = $limit1 - 20;
-					 #	$limit2 = ($limit2 < 0) ? 0 : $limit2;
-						$cb_select = $db->query('SELECT * FROM cb_test WHERE del = 0 AND salon = ""  ORDER BY post_date DESC LIMIT 0, 20');
+						$cb_select = $db->query('SELECT * FROM cb_test WHERE del = 0 AND salon = ""  ORDER BY post_date DESC LIMIT 20');
 					}
 				}
 				else
-				{
-					#$count = $db->prepare('SELECT COUNT(*) AS count FROM cb_test WHERE del = 0 AND salon = "" AND to_id = ? OR
-					 #del = 0 AND salon = "" AND to_id = 0  ORDER BY post_date ASC ');
-					 #	$count->execute(array($_SESSION['id'])); $count = $count->fetch();
-					 #	$limit1 = $count['count'];
-					 #	$limit2 = $limit1 - 20;
-					 #	$limit2 = ($limit2 < 0) ? 0 : $limit2;
-					 	
+				{	
 					$cb_select = $db->prepare('SELECT * FROM cb_test WHERE del = 0 AND salon = "" AND to_id = ? OR
-					 del = 0 AND salon = "" AND to_id = 0  ORDER BY post_date DESC LIMIT 0, 20');
+					 del = 0 AND salon = "" AND to_id = 0  ORDER BY post_date DESC LIMIT 20');
 					 $cb_select->execute(array($_SESSION['id']));	
 				}
 				 
@@ -89,8 +75,13 @@
 				 
 					 if ($line['del'] == 0)	
 					 {
+					 
+					 	if ($_SESSION['rank'] >= $name_cb['rank'])
+					 	{
+					 		$delbutton = '<a href="index?p=cb&del='. $line['id'].'" style="color:red;">[x]</a> ';
+					 	}
 						?>
-						<p style="text-align:left;"> <a href="index?p=cb&del=<?= $line['id']?>" style="color:red;">[x]</a> 
+						<p style="text-align:left;"> <?= $delbutton ?>
 						[<?= $date_send; ?>] <img src="pics/avatar/miniskin_<?= $line['sender_id']?>.png" alt="" width="15px" />
 						<span class="name<?= $name_cb['rank'], $tech, $pionier; ?>"><?= $name_cb['name']?></span> : <span <?= $effect
 						?>><?= $line['message']?></span>
