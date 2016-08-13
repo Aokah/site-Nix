a39dc86b31487cd906c07855ed3c18a54f087a9b
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-13 17:51:43 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -11,14 +11,30 @@
 	
 	#$api->call("runConsoleCommand", array("nick Jolfrid &3 Tya"));
 	$mc = "Nikho_Gabriel";
-	$string = 'execute @@ ~ ~ ~ weather thunder';
+	$string = 'execute @@ ~ ~ ~ weather clear';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
 	$replacements[0] = "$mc";
-	$command = preg_replace($patterns, $replacements, $string);
-	echo $command;
-	$api->call("runConsoleCommand", array("$command"));
+	$command1 = preg_replace($patterns, $replacements, $string);
+	$api->call("runConsoleCommand", array("$command1"));
+	
+	$string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force';
+	$patterns = array();
+	$patterns[0] = '/@@/';
+	$replacements = array();
+	$replacements[0] = "$mc";
+	$command3 = preg_replace($patterns, $replacements, $string);
+	$api->call("runConsoleCommand", array("$command3"));
+	
+	
+	$string = 'execute @@ ~ ~ ~ tellraw @a ["",{"text":"[","color":"white"},{"text":"Console","color":"gray"},{"text":"]","color":"white"},{"text":" ** Le temps s\'éclaircit soudainement ! **","color":"aqua"}]';
+	$patterns = array();
+	$patterns[0] = '/@@/';
+	$replacements = array();
+	$replacements[0] = "$mc";
+	$command2 = preg_replace($patterns, $replacements, $string);
+	$api->call("runConsoleCommand", array("$command2"));
 	
 }
 ?>
