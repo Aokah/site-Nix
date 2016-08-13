0f0f08a43a6755d7c2693dd22a121cc4255b6a12
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-13 19:09:43 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -10,7 +10,7 @@
 		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
 	
 	$mc = "chugo";
-	$string = 'execute @@ ~ ~ ~ weather thunder';
+	$string = 'execute @@ ~ ~ ~ weather rain';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
@@ -18,16 +18,37 @@
 	$command1 = preg_replace($patterns, $replacements, $string);
 	$api->call("runConsoleCommand", array("$command1"));
 	
-	/*$string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force';
+	$type = 4;
+	switch ($type)
+	{
+		case 1: $string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force'; break;
+		case 2: $string = 'execute @@ ~ ~ ~ particle enchantmenttable ~ ~1 ~ 0.8 1 0.8 1 300 force'; break;
+		case 3: $string = 'execute @@ ~ ~ ~ particle townaura ~ ~1 ~ 0.5 0.5 0.5 0.001 300 force'; break;
+		case 4: $string = 'execute @@ ~ ~ ~ particle dripWater ~ ~1 ~ 1 1 1 0.1 300 force'; break;
+		case 5: $string = 'no'; break;
+		case 6: $string = 'execute @@ ~ ~ ~ particle flame ~ ~1 ~ 1 1.5 1 0.01 100 force'; break;
+		case 7: $string = 'execute @@ ~ ~ ~ particle snowshovel ~ ~1 ~ 1 1.5 1 0.01 200 force'; break;
+		case 8: $string = 'execute @@ ~ ~ ~ particle endRod ~ ~1 ~ 1 1 1 0.01 100 force'; break;
+		case 9: $string = 'execute @@ ~ ~ ~ particle lava ~ ~1 ~ 1 1.5 1 0.1 100 force'; break;
+		case 10: $string = 'execute @@ ~ ~ ~ particle happyVillager ~ ~1 ~ 1 1.5 1 0.01 200 force'; break;
+		case 11: $string = 'execute @@ ~ ~ ~ particle smoke ~ ~1 ~ 0.5 1 0.5 0.001 200 force'; break;
+		case 12: $string = 'execute @@ ~ ~ ~ particle magicCrit ~ ~1 ~ 1 1.5 1 0.1 150 force'; break;
+		case 13: $string = 'execute @@ ~ ~ ~ particle crit ~ ~1 ~ 1 1.5 1 0.01 200 force'; break;
+		case 14: $string = 'execute @@ ~ ~ ~ particle dripLava ~ ~1 ~ 1 1.5 1 0.1 100 force'; break;
+		case 15: $string = 'execute @@ ~ ~ ~ particle instantSpell ~ ~1 ~ 1 1.5 1 0.01 200 force'; break;
+		case 16: $string = 'no'; break;
+		case 17: $string = 'execute @@ ~ ~ ~ particle dragonbreath ~ ~1 ~ 1 1 1 0.01 100 force'; break;
+	}
+	$string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
 	$replacements[0] = "$mc";
 	$command3 = preg_replace($patterns, $replacements, $string);
-	$api->call("runConsoleCommand", array("$command3"));*/
+	$api->call("runConsoleCommand", array("$command3"));
 	
 	
-	$string = 'execute @@ ~ ~ ~ tellraw @a ["",{"text":"[","color":"white"},{"text":"Console","color":"gray"},{"text":"]","color":"white"},{"text":" ** Le temps devient soudainement orageux ! **","color":"aqua"}]';
+	$string = 'execute @@ ~ ~ ~ tellraw @a ["",{"text":"[","color":"white"},{"text":"Console","color":"gray"},{"text":"]","color":"white"},{"text":" ** Le temps devient soudainement pluvieux ! **","color":"aqua"}]';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
