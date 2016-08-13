26ec278abe385619e3ffa0d9cab08aa25bcf121d
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-13 18:07:29 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -9,9 +9,8 @@
 		$salt = 'salt';
 		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
 	
-	#$api->call("runConsoleCommand", array("nick Jolfrid &3 Tya"));
-	$mc = "Nikho_Gabriel";
-	$string = 'execute @@ ~ ~ ~ weather clear';
+	$mc = "chugo";
+	$string = 'execute @@ ~ ~ ~ weather thunder';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
@@ -19,16 +18,16 @@
 	$command1 = preg_replace($patterns, $replacements, $string);
 	$api->call("runConsoleCommand", array("$command1"));
 	
-	$string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force';
+	/*$string = 'execute @@ ~ ~ ~ particle cloud ~ ~1 ~ 1 2 1 0.1 200 force';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
 	$replacements[0] = "$mc";
 	$command3 = preg_replace($patterns, $replacements, $string);
-	$api->call("runConsoleCommand", array("$command3"));
+	$api->call("runConsoleCommand", array("$command3"));*/
 	
 	
-	$string = 'execute @@ ~ ~ ~ tellraw @a ["",{"text":"[","color":"white"},{"text":"Console","color":"gray"},{"text":"]","color":"white"},{"text":" ** Le temps s\'éclaircit soudainement ! **","color":"aqua"}]';
+	$string = 'execute @@ ~ ~ ~ tellraw @a ["",{"text":"[","color":"white"},{"text":"Console","color":"gray"},{"text":"]","color":"white"},{"text":" ** Le temps devient soudainement orageux ! **","color":"aqua"}]';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
