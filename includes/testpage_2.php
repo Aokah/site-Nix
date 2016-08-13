db6d01921cf584e0d0308d514c62a0b4201d968c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-13 17:12:59 +0200

    Update testpage_2.php

--- includes/testpage_2.php
+++ includes/testpage_2.php
@@ -10,13 +10,15 @@
 		$api = new JSONAPI($ip, $port, $user, $pwd, $salt);
 	
 	#$api->call("runConsoleCommand", array("nick Jolfrid &3 Tya"));
-	$mc = "Nikh_Gabriel";
+	$mc = "Nikho_Gabriel";
 	$string = 'execute @@ ~ ~ ~ weather clear';
 	$patterns = array();
 	$patterns[0] = '/@@/';
 	$replacements = array();
 	$replacements[0] = "/$mc/";
-	echo preg_replace($patterns, $replacements, $string);
+	$command = preg_replace($patterns, $replacements, $string);
+	
+	$api->call("runConsoleCommand", array("$command"));
 	
 }
 ?>
