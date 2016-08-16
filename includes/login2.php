547d861f1c977ca376c17d8a2cb4efc1677d3a6d
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-16 21:10:00 +0200

    Update login2.php

--- includes/login2.php
+++ includes/login2.php
@@ -4,16 +4,28 @@
   
   
 	$ip = $_SERVER['REMOTE_ADDR'];
-	
+	$verif = $db-prepare('SELECT * FROM blacklist WHERE ip = ?'); $verif->execute(array($ip));
 	?>
-	<div class="navtitle">Connexion</div>
+	<div class="navtitle">Connexion (Indev)</div>
 		<ul class="nav">
 			<li class="navbg2" style="clear: both;;list-style-type: none;">
-				<form action="index?p=<?= $_GET['p']?>" method="POST">
-					<label for="user">Identifiant : </label><input type="text" name="user" id="user"/><br/>
-					<label for="pass">Mot de Passe : </label><input type="password"  id="pass" name="pass" /><br />
-					<input type="submit" name="login" />
-				</form>
+				<?php /*if ($verif->fetch())
+				{*/
+				?>
+					<p>Votre adresse IP figure parmis les adresses bannies, vous ne pouvez donc pas vous conneter.</p>
+				<?php /*
+				}
+				else
+				{
+				?>
+					<form action="index?p=<?= $_GET['p']?>" method="POST">
+						<label for="user">Identifiant : </label><input type="text" name="user" id="user"/><br/>
+						<label for="pass">Mot de Passe : </label><input type="password"  id="pass" name="pass" /><br />
+						<input type="submit" name="login" />
+					</form>
+				<?php
+			*/	}
+				?>
 			</li>
 		</ul>
 	<?php
