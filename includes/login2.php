f3b2a9417e29701e9567761c219019e72a3b6d93
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-16 21:00:49 +0200

    Update login2.php

--- includes/login2.php
+++ includes/login2.php
@@ -9,7 +9,7 @@
 	<div class="navtitle">Connexion</div>
 		<ul class="nav">
 			<li class="navbg2" style="clear: both;;list-style-type: none;">
-				<form action="index?p=<?= $_GET['p']?>" method="POST" style="text-align:justify;">
+				<form action="index?p=<?= $_GET['p']?>" method="POST">
 					<label for="user">Identifiant : </label><input type="text" name="user" id="user"/><br/>
 					<label for="pass">Mot de Passe : </label><input type="password"  id="pass" name="pass" /><br />
 					<input type="submit" name="login" />
