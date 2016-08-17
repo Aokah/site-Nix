50e9dd4975c513c33ab2fbd658d91045f77bbd32
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-17 04:00:45 +0200

    Update login2.php

--- includes/login2.php
+++ includes/login2.php
@@ -20,8 +20,37 @@
 				{
 					if (isset($_POST['login']))
 					{
-						#$name = htmlspecialchars
+						$name = htmlspecialchars($_POST['user']);
 						$select = $db->prepare('SELECT * FROM members WHERE name = ?');
+						$select->execute(array($name));
+						if ($line = $select->fetch())
+						{
+							if (password_verify($_POST['pwd'], $line['password']))
+							{
+								if ($line['ban'] == 1)
+								{
+									echo "Navré, mais ce compte est banni du site.";
+								}
+								elseif ($line['removed'] == 1)
+								{
+									echo "Navré, mais ce compte a été supprimé.";
+								}
+								elseif ($line['end'] == 1)
+								{
+									echo "Navré, mais votre compte a été vérouillé après avoir eu une excellente fin, vous pouvez toujours demander un compte au même rang pour continuer à avoir accès au site.";
+								}
+								else
+								{
+									$_SESSION['connected'] = true;
+									$_SESSION['name'] = $line['name'];
+									$_SESSION['id'] = $line['id'];
+									$_SESSION['rank'] = $line['rank'];
+									$_SESSION['title'] = $line['title'];
+									
+									echo "Connexion effectuée <span class='name", $line['rank'],"'>", $line['name'], " !";
+								}
+							}
+						}
 					}
 					else
 					{
