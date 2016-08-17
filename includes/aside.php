94b534436ef6b6362abd956ce01eb9c61a964bcb
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-17 10:26:45 +0200

    Update aside.php

--- includes/aside.php
+++ includes/aside.php
@@ -12,8 +12,9 @@
 				
 	$grada = $db->query('SELECT * FROM hist_grada ORDER BY id DESC LIMIT 10');
 	
-	if ($_SESSION['connected'])
+	if (!$_SESSION['connected'])
 	{
+		echo 'blblbl';
 	}
 	else
 	{
