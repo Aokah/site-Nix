cba587d967e0eeedba4e51cd3ed3e9b352f0ab9c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-19 15:41:16 +0200

    Update testpage_3.php

--- includes/testpage_3.php
+++ includes/testpage_3.php
@@ -8,7 +8,140 @@
 	
 	if (isset($_GET['cat']))
 	{
+		$cat = intval($_GET['cat']);
+		$verify = $db->prepare('SELECT * FROM forum_category WHERE id = ?'); $verify->execute(array($cat));
+		$verify = $verify->fetch()
 		
+		if ($verif['rank'] <= $view)
+		{
+			if ($view < 6)
+			{
+				$flist = $db->prepare('SELECT * FROM forum_forum WHERE category = ? AND del = 0');
+				$flist->execute(array($cat));
+			}
+			else
+			{
+				$flist = $db->prepare('SELECT * FROM forum_forum WHERE category = ?');
+				$flist->execute(array($cat));
+			}
+			$select = $db->prepare('SELECT * FROM forum_category WHERE id = ?'); $select->execute(array($cat));
+			?>
+			<div width="100%" style="padding:1%" class="memberbg_7">
+				<?php
+				while ($line = $select->fetch())
+				{
+					
+				?>
+				<h4><a href="index?p=testpage_3&cat=<?= $line['id']?>"><?= $line['name']?></a></h4>
+				<p><img src="pics/forumcat_<?= $line['id']?>.png" class="guild" /></p>
+				<table cellspacing="0" cellpadding="1%" align="center" width="95%">
+					<tbody>
+						<tr class="member_top">
+							<th>Sujet</th> <th width="25%">Dernière activité</th>
+						</tr>
+						<?php 
+						while ($list = $flist->fetch())
+						{
+							$important = ($list['important'] == 1) ? '<span style="color:gold;">[Important]</span> ' : "";
+							$verif = $db->prepare('SELECT * FROM forum_unread WHERE user_id = ? AND forum_id = ?');
+							$verif->execute(array($_SESSION['id'], $list['id']));
+							if ($verif = $verif->fetch())
+							{
+								if ($verif['unread'] == 1)
+								{
+									$read = "";
+									$page = $verif['page'];
+								}
+								else
+								{
+									$read = "class=\"unread\"";
+									$page = $verif['page'];
+								}
+							}
+							else
+							{
+								$read = "class=\"unread\"";
+								$page = 1;
+							}
+							if ($list['important'] == 0)
+							{
+								$imp = "<a href=\"index?p=testpage_3&imp=". $list['id']. "\" style=\"color:gold;\">[I]</a>";
+							}
+							else
+							{
+								$imp = "<a href=\"index?p=testpage_3&norm=". $list['id']. "\" style=\"color:blue;\">[N]</a>";
+							}
+							$rp = ($list['rp'] == 1) ? "<span style='color:lime;'> [RP] </span>" : "";
+							
+							$latest = $db->prepare('SELECT * FROM forum_post WHERE forum_id = ? ORDER BY id DESC'); $latest->execute(array($list['id']));
+							if ($latest = $latest->fetch())
+							{
+								if ($latest['unknow'] == 0)
+								{
+									$member = $db->prepare('SELECT * FROM members WHERE id = ?'); $member->execute(array($latest['user_id']));
+									$member = $member->fetch();
+									$title = $member['title'];
+									$title = ($member['pionier']== 1)? "Pionier" : $title;
+									$title = ($member['ban'] == 1)? "Banni" : $title;
+									$title = ($members['removed'] == 1)? "Oublié" : $title;
+									$user = $member['name'];
+									$tech = ($member['technician'] == 1)? "-T" : "";
+									$pionier = ($member['pionier'] == 1)? "-P" : "";
+									$color = $member['rank']. "" . $tech. "" . $pionier;
+									$a = "<a class='name". $color ."' href='index?p=perso&perso=" . $member['id'] ."'>";
+									$aend = "</a>";
+									$img = "<img src='pics/avatar/miniskin_" . $latest['user_id'] . ".png' alt='' width='6%' />";
+								}
+								else
+								{
+									$title = "Message";
+									$user = "Anonyme";
+									$color = "1";
+									$a = "<span class='name" . $color . "'>";
+									$aend = "</span>";
+									$img = "";
+								}
+								$date = preg_replace('#^(.{4})-(.{2})-(.{2}) (.{2}:.{2}):.{2}$#', 'Le $3/$2/$1 à $4', $latest['post_date']);
+								
+								$last = $img ." ". $a ."" . $title . " ". $user. "". $aend ."<br />" . $date ."";
+							}
+							else
+							{
+								$last = "Aucun message dans ce forum.";
+							}
+							$del = ($flist['del'] == 1)? "<span color:red;>[Supprimé] </span>" : "";
+						?>
+						<tr class="memberbg_5">
+							<td <?= $read?>>
+								<?php 
+								if ($view > 5)
+								{
+								?>
+								<a href="index?p=testpage_3&del=<?=$list['id']?>" style="color:red;">[X]</a> <?= $imp?> |
+								<?
+								}
+								?>
+								<a href="index?p=testpage_3&forum=<?=$list['id']?>&page=1"><?=$del, $important, $rp , $list['name']?></a>
+							</td>
+							
+							<td <?= $read?>><?= $last ?></td>
+						</tr>
+						<?php
+						}
+						?>
+						<tr>
+							
+						</tr>
+					</tbody>
+				</table>
+			<?php
+		}
+		else
+		{
+		?>
+			<p>Vous n'avez pas les droits nécessaires pour visinner cette catégorie.</p>
+		<?php
+		}
 	}
 	elseif (isset($_GET['forum']))
 	{
