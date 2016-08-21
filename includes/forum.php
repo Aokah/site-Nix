abd5f98a2c55a3ebb2e9d44345ee956a58dc4529
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-21 21:21:08 +0200

    Update forum.php

--- includes/forum.php
+++ includes/forum.php
@@ -341,7 +341,7 @@
 			{
 				$fcount = $db->prepare('SELECT COUNT(*) AS pages FROM forum_post WHERE forum_id = ?'); $fcount->execute(array($forum));
 				$fcount = $fcount->fetch();
-				$plimit = floor($fcount['pages'] / 10); echo $plimit;
+				$plimit = floor($fcount['pages'] / 10); echo $plimit, $fcount['pages'];
 				$page = ($page > $plimit)? $plimit : $page;
 				$pmin = ($page*10)-10;
 				$pmax = $page*10;
