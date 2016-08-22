3e2add00ac29cd8a0f4796b1b8904eca23cd183c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-22 19:17:50 +0200

    Update forum_alert.php

--- includes/forum_alert.php
+++ includes/forum_alert.php
@@ -1,5 +1,30 @@
 <?php function unread()
 {
-
+	global $db;
+	
+	$forum = intval($_GET['forum']);
+	
+	$verif = $db->prepare('SELECT * FROM forum_unread WHERE forum_id = ? AND user_id = ?');
+	$verif->execute(array($forum, $_SESSION['id']));
+	if ($verif->fetch())
+	{
+		echo "La ligne existe";
+	}
+	else
+	{
+		echo "la ligne n'existe pas";
+	}
+}
+function post()
+{
+	global $db;
+	
+	$forum = intval($_GET['forum']);
+}
+function page_f()
+{
+	global $db;
+	
+	$forum = intval($_GET['forum']);
 }
 ?>
