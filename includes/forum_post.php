e8375c5efbba8f45d9741f7393328644c805dd5c
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-22 20:45:58 +0200

    Create forum_post.php

---
+++ includes/forum_post.php
@@ -0,0 +1,9 @@
+function post()
+{
+	global $db;
+	
+	$forum = intval($_GET['forum']);
+	
+	$update = $db->prepare('UPDATE forum_unread SET unread = 0 WHERE forum_id = ?');
+	$update->execute(array($forum));
+}
