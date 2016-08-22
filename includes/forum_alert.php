7090bfff34978856e2e067e040a96132d2057c5a
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-22 20:38:39 +0200

    Update forum_alert.php

--- includes/forum_alert.php
+++ includes/forum_alert.php
@@ -8,23 +8,30 @@
 	$verif->execute(array($forum, $_SESSION['id']));
 	if ($verif->fetch())
 	{
-		echo "La ligne existe";
+		$update = $db->prepare('UPDATE forum_unread SET unread = 1 WHERE forum_id = ? AND user_id= ?');
+		$update->execute(array($forum,$_SESSION['id']));
 	}
 	else
 	{
-		echo "la ligne n'existe pas";
+		$add = $db->prepare('INSERT INTO forum_unread VALUES("", ?, ?, 0, 1)');
+		$add->execute(array($forum, $_SESSION['id{']));
 	}
+	$curpage = ($_GET['page'] > 1)? intval($_GET['page']) : 1;
+	
+	$verify = $db->prepare('SELECT page FROM forum_unread WHERE user_id = ? AND forum_id = ?');
+	$verify->execute(array($_SESSION['id'],$forum)); $verif = $verify->fetch();
+	if ($curpage > $verif['page'])
+	{
+		$page = $db->prepare('UPDATE forum_unread SET page = ?');
+		$page->execute(array($curpage));
 }
 function post()
 {
 	global $db;
 	
 	$forum = intval($_GET['forum']);
-}
-function page_f()
-{
-	global $db;
 	
-	$forum = intval($_GET['forum']);
+	$update = $db->prepare('UPDATE forum_unread SET unread = 0 WHERE forum_id = ?');
+	$update->execute(array($forum));
 }
 ?>
