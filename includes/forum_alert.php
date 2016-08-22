bc1b926471019a307a4638b35807d3d5e28e18fa
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-08-22 20:45:29 +0200

    Update forum_alert.php

--- includes/forum_alert.php
+++ includes/forum_alert.php
@@ -26,13 +26,4 @@
 		$page->execute(array($curpage));
 	}
 }
-function post()
-{
-	global $db;
-	
-	$forum = intval($_GET['forum']);
-	
-	$update = $db->prepare('UPDATE forum_unread SET unread = 0 WHERE forum_id = ?');
-	$update->execute(array($forum));
-}
 ?>
